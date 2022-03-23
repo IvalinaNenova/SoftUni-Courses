1b39acc82f544cecf8e6c3d13f5d3d01aa7194ce
Ivalina Nenova <nenovaivalina@gmail.com>
2022-03-23 16:56:06 +0200

    Added More-Exercise solution

---
+++ Basics/First-Steps-In-Coding/More-Exercise/T05.TrainingLab/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace T05.TrainingLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+
+            double freeWidth = width - 1;
+            double desksInWidth = Math.Floor(freeWidth / 0.7);
+            double desksInLenght = Math.Floor(lenght / 1.2);
+            double totalDesks = desksInWidth * desksInLenght - 3;
+            Console.WriteLine(totalDesks);
+
+
+        }
+    }
+}
