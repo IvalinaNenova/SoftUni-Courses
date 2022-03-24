6bff894998db5de42c253ef65411b0df2d0d8b63
Ivalina Nenova <nenovaivalina@gmail.com>
2022-03-24 13:45:35 +0200

    Adding more files

---
+++ Basics/First-Steps-In-Coding/Exercise/FirstStepsInCodingExersise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FirstStepsInCodingExersise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //: 1 USD = 1.79549 BGN.
+
+            double USD = double.Parse(Console.ReadLine());
+            double BGN = USD * 1.79549;
+
+            Console.WriteLine(BGN);
+        }
+    }
+}
---
+++ Basics/First-Steps-In-Coding/Lab/FirstStepsInCoding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstStepsInCoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
