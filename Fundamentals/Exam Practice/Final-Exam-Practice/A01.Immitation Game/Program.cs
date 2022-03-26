309b4e6fbba9a6b290dfbde14f835d5c29723823
Ivalina Nenova <nenovaivalina@gmail.com>
2022-03-26 12:53:42 +0200

    Create new solution

---
+++ Fundamentals/Exam Practice/Final-Exam-Practice/A01.Immitation Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A01.Immitation_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
