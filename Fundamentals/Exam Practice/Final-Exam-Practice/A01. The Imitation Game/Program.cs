7b8fc908318aef2d3e4602e8a7e00fc1aeb0797d
Ivalina Nenova <nenovaivalina@gmail.com>
2022-03-31 11:07:28 +0300

    Created Final-Exam-Practice Solution

---
+++ Fundamentals/Exam Practice/Final-Exam-Practice/A01. The Imitation Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A01._The_Imitation_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
