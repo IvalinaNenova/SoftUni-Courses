370724b586412073ee91241164c8d8f6f62cfcb4
Ivalina Nenova <nenovaivalina@gmail.com>
2022-03-31 11:27:48 +0300

    Created new project

--- Fundamentals/Exam Practice/Final-Exam-Practice/A01. The Imitation Game/Program.cs
+++ Fundamentals/Exam Practice/Final-Exam-Practice/A01. The Imitation Game/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 
 namespace A01._The_Imitation_Game
 {
@@ -6,7 +8,39 @@ namespace A01._The_Imitation_Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string encryptedMessage = Console.ReadLine();
+            string inputCommands = Console.ReadLine();
+
+            StringBuilder decryptedMessage = new StringBuilder(encryptedMessage);
+
+            while (inputCommands != "Decode")
+            {
+                string[] commands = inputCommands.Split("|");
+                string action = commands[0];
+
+                if (action == "Move")
+                {
+                    int countOfLetters = int.Parse(commands[1]);
+                    decryptedMessage.Append(decryptedMessage,0,countOfLetters)
+                                    .Remove(0, countOfLetters);
+                }
+                else if (action == "Insert")
+                {
+                    int index = int.Parse(commands[1]);
+                    string value = commands[2];
+                    decryptedMessage.Insert(index, value);
+                }
+                else if (action == "ChangeAll")
+                {
+                    string oldString = commands[1];
+                    string newString = commands[2];
+                    decryptedMessage.Replace(oldString, newString);
+                }
+                
+                inputCommands = Console.ReadLine();
+            }
+
+            Console.WriteLine($"The decrypted message is: {decryptedMessage.ToString()}");
         }
     }
 }
