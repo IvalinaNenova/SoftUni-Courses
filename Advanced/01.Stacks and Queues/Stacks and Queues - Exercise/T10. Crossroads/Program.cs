04394d6e119c024466e800e5b44ab16caf9a8965
Ivalina Nenova <nenovaivalina@gmail.com>
2022-04-20 16:38:04 +0300

    Added solution to task 10

--- Advanced/01.Stacks and Queues/Stacks and Queues - Exercise/T10. Crossroads/Program.cs
+++ Advanced/01.Stacks and Queues/Stacks and Queues - Exercise/T10. Crossroads/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace T10._Crossroads
 {
@@ -6,7 +9,59 @@ namespace T10._Crossroads
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int greenLight = int.Parse(Console.ReadLine());
+            int freeWindow = int.Parse(Console.ReadLine());
+            int countOfCarsPassed = 0;
+
+            Queue<string> trafficQueue = new Queue<string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                if (input != "green")
+                {
+                    trafficQueue.Enqueue(input);
+                }
+                else
+                {
+                    int currentGreenLight = greenLight;
+
+                    while (trafficQueue.Any() && currentGreenLight > 0)
+                    {
+                        string currentCar = trafficQueue.Dequeue();
+                        int timeNeeded = currentCar.Length;
+
+                        if (currentGreenLight - timeNeeded >= 0)
+                        {
+                            currentGreenLight -= timeNeeded;
+                            countOfCarsPassed++;
+                        }
+                        else if (currentGreenLight + freeWindow - timeNeeded >= 0)
+                        {
+                            countOfCarsPassed++;
+                            freeWindow -= timeNeeded - greenLight;
+                            currentGreenLight = 0;
+                        }
+                        else
+                        {
+                            int index = freeWindow + currentGreenLight;
+
+                            Console.WriteLine("A crash happened!");
+                            Console.WriteLine($"{currentCar} was hit at {currentCar[index]}.");
+                            return;
+                        }
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (input == "END")
+            {
+                Console.WriteLine("Everyone is safe.");
+                Console.WriteLine($"{countOfCarsPassed} total cars passed the crossroads.");
+            }
         }
     }
 }
