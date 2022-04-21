37e3078fb22732f0a4a86b172f047e056144b5fa
Ivalina Nenova <nenovaivalina@gmail.com>
2022-04-21 14:19:26 +0300

    Added Solutions to the last two tasks

--- Advanced/01.Stacks and Queues/Stacks and Queues - Exercise/T11. Key Revolver/Program.cs
+++ Advanced/01.Stacks and Queues/Stacks and Queues - Exercise/T11. Key Revolver/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace T11._Key_Revolver
 {
@@ -6,7 +9,48 @@ namespace T11._Key_Revolver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int sizeOfBarrel = int.Parse(Console.ReadLine());
+            int[] bulletsArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] locksArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int intelligenceValue = int.Parse(Console.ReadLine());
+            int currentBulletsInBarel = sizeOfBarrel;
+
+            Queue<int> locks = new Queue<int>(locksArray);
+            Stack<int> bullets = new Stack<int>(bulletsArray);
+
+            while (bullets.Any() && locks.Any())
+            {
+                int currentBullet = bullets.Pop();
+                currentBulletsInBarel--;
+                
+                if (currentBullet <= locks.Peek() )
+                {
+                    Console.WriteLine("Bang!");
+                    locks.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+
+                if (currentBulletsInBarel == 0 && bullets.Any())
+                {
+                    Console.WriteLine("Reloading!");
+                    currentBulletsInBarel = sizeOfBarrel;
+                }
+            }
+
+            if (locks.Any())
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+            }
+            else
+            {
+                int bulletsUsed = bulletsArray.Length - bullets.Count;
+                int deduction = bulletsUsed * bulletPrice;
+                Console.WriteLine($"{bullets.Count} bullets left. Earned ${intelligenceValue - deduction}");
+            }
         }
     }
 }
--- Advanced/01.Stacks and Queues/Stacks and Queues - Exercise/T12. Cups and Bottles/Program.cs
+++ Advanced/01.Stacks and Queues/Stacks and Queues - Exercise/T12. Cups and Bottles/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Channels;
 
 namespace T12._Cups_and_Bottles
 {
@@ -6,7 +10,32 @@ namespace T12._Cups_and_Bottles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] cupsCapacities = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] bottlesArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Queue<int> cups = new Queue<int>(cupsCapacities);
+            Stack<int> bottles = new Stack<int>(bottlesArray);
+
+            int wastedWater = 0;
+
+            while (cups.Any() && bottles.Any())
+            {
+                int currentCupCapacity = cups.Peek();
+
+                while (currentCupCapacity > 0)
+                {
+                    currentCupCapacity -= bottles.Pop();
+                }
+
+                cups.Dequeue();
+                wastedWater += Math.Abs(currentCupCapacity);
+            }
+
+            Console.WriteLine(cups.Any() 
+                ? $"Cups: {string.Join(' ', cups)}" 
+                : $"Bottles: {string.Join(' ', bottles)}");
+
+            Console.WriteLine($"Wasted litters of water: {wastedWater}");
         }
     }
 }
