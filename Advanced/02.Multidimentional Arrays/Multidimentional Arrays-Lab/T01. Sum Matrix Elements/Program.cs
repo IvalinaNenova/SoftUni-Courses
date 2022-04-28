9fffb06f7ba2b4459e40b25e9bc92c4c94c5c1c3
Ivalina Nenova <nenovaivalina@gmail.com>
2022-04-28 16:33:49 +0300

    Created Multidimentional Array - Lab Solution

---
+++ Advanced/02.Multidimentional Arrays/Multidimentional Arrays-Lab/T01. Sum Matrix Elements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T01._Sum_Matrix_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
