be1685886c65ee304e818ca98f6ed04ebf8117da
Ivalina Nenova <nenovaivalina@gmail.com>
2022-04-28 16:50:44 +0300

    Added Task 02

--- Advanced/02.Multidimentional Arrays/Multidimentional Arrays-Lab/T02. Sum Matrix Columns/Program.cs
+++ Advanced/02.Multidimentional Arrays/Multidimentional Arrays-Lab/T02. Sum Matrix Columns/Program.cs
@@ -6,7 +6,33 @@ namespace T02._Sum_Matrix_Columns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] matrixParams = Console.ReadLine().Split(", ");
+            int rows = int.Parse(matrixParams[0]);
+            int cols = int.Parse(matrixParams[1]);
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] numbers = Console.ReadLine().Split(" ");
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = int.Parse(numbers[col]);
+                }
+            }
+
+            for (int col = 0; col < cols; col++)
+            {
+                int colSum = 0;
+
+                for (int row = 0; row < rows; row++)
+                {
+                    colSum+= matrix[row, col];
+                }
+
+                Console.WriteLine(colSum);
+            }
         }
     }
 }
