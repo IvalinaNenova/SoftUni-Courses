596bd7a69f9bc78b0913b18a2959acc8ebd1dfff
Ivalina Nenova <nenovaivalina@gmail.com>
2022-04-28 16:45:00 +0300

    Updated task 01

--- Advanced/02.Multidimentional Arrays/Multidimentional Arrays-Lab/T01. Sum Matrix Elements/Program.cs
+++ Advanced/02.Multidimentional Arrays/Multidimentional Arrays-Lab/T01. Sum Matrix Elements/Program.cs
@@ -6,7 +6,25 @@ namespace T01._Sum_Matrix_Elements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] parameters = Console.ReadLine().Split(", ");
+            int rows = int.Parse(parameters[0]);
+            int cols = int.Parse(parameters[1]);
+            int[,] matrix = new int[rows, cols];
+            int sum = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                string[] numbers = Console.ReadLine().Split(", ");
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = int.Parse(numbers[col]);
+                    sum += int.Parse(numbers[col]);
+                }
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
         }
     }
 }
--- Advanced/02.Multidimentional Arrays/Multidimentional Arrays-Lab/T07. Pascal Triangle/Program.cs
+++ Advanced/02.Multidimentional Arrays/Multidimentional Arrays-Lab/T07. Pascal Triangle/Program.cs
@@ -7,25 +7,7 @@ namespace T07._Pascal_Triangle
     {
         static void Main(string[] args)
         {
-            string[] parameters = Console.ReadLine().Split(", ");
-            int rows = int.Parse(parameters[0]);
-            int cols = int.Parse(parameters[1]);
-            int[,] matrix = new int[rows, cols];
-            int sum = 0;
-            for (int row = 0; row < rows; row++)
-            {
-                string[] numbers = Console.ReadLine().Split(", ");
-
-                for (int col = 0; col < cols; col++)
-                {
-                    matrix[row, col] = int.Parse(numbers[col]);
-                    sum += int.Parse(numbers[col]);
-                }
-            }
-
-            Console.WriteLine(matrix.GetLength(0));
-            Console.WriteLine(matrix.GetLength(1));
-            Console.WriteLine(sum);
+            
         }
     }
 }
