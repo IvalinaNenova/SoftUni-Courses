e4e84d9cd401eeda717e1632ea6ad6644ba45dbf
Ivalina Nenova <nenovaivalina@gmail.com>
2022-04-28 17:30:08 +0300

    Added solutions to tasks 04 and 05

--- Advanced/02.Multidimentional Arrays/Multidimentional Arrays-Lab/T04. Symbol in Matrix/Program.cs
+++ Advanced/02.Multidimentional Arrays/Multidimentional Arrays-Lab/T04. Symbol in Matrix/Program.cs
@@ -6,7 +6,39 @@ namespace T04._Symbol_in_Matrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                string text = Console.ReadLine();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = text[col];
+                }
+            }
+
+            char symbolToLookFor = char.Parse(Console.ReadLine());
+            bool isFound = false;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (matrix[row,col] == symbolToLookFor)
+                    {
+                        isFound = true;
+                        Console.WriteLine($"({row}, {col})");
+                        return;
+                    }
+                }
+            }
+
+            if (!isFound)
+            {
+                Console.WriteLine($"{symbolToLookFor} does not occur in the matrix");
+            }
         }
     }
 }
--- Advanced/02.Multidimentional Arrays/Multidimentional Arrays-Lab/T05. Square With Maximum Sum/Program.cs
+++ Advanced/02.Multidimentional Arrays/Multidimentional Arrays-Lab/T05. Square With Maximum Sum/Program.cs
@@ -6,7 +6,52 @@ namespace T05._Square_With_Maximum_Sum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] matrixParameters = Console.ReadLine().Split(", ");
+            int rows = int.Parse(matrixParameters[0]);
+            int cols = int.Parse(matrixParameters[1]);
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] numbers = Console.ReadLine().Split(", ");
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = int.Parse(numbers[col]);
+                }
+            }
+
+            int highestSum = int.MinValue;
+            int highestSumRow = 0;
+            int highestSumCol = 0;
+            for (int i = 0; i < rows-1; i++)
+            {
+                for (int j = 0; j < cols-1; j++)
+                {
+                    int subMatrixSum = matrix[i, j] + matrix[i,j+1] +
+                                        matrix[i+1,j] + matrix[i+1,j+1];
+
+                    if (subMatrixSum > highestSum)
+                    {
+                        highestSum = subMatrixSum;
+                        highestSumRow = i;
+                        highestSumCol = j;
+                    }
+                }
+            }
+
+            for (int row = highestSumRow; row < highestSumRow+2; row++)
+            {
+                for (int col = highestSumCol; col < highestSumCol+2; col++)
+                {
+                    Console.Write(matrix[row,col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(highestSum);
         }
     }
 }
