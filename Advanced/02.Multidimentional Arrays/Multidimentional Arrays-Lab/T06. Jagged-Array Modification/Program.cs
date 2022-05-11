baa77f457cf57fea0c78bac11ef6a022f775d690
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-11 15:09:54 +0300

    Added the solutions to problems 06 and 07

--- Advanced/02.Multidimentional Arrays/Multidimentional Arrays-Lab/T06. Jagged-Array Modification/Program.cs
+++ Advanced/02.Multidimentional Arrays/Multidimentional Arrays-Lab/T06. Jagged-Array Modification/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace T06._Jagged_Array_Modification
 {
@@ -6,7 +7,54 @@ namespace T06._Jagged_Array_Modification
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int rows = int.Parse(Console.ReadLine());
+            int[][] arrays = new int[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                arrays[i] = new int[numbers.Length];
+
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    arrays[i][j] = numbers[j];
+                }
+            }
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] commands = input.Split(' ');
+                int row = int.Parse(commands[1]);
+                int col = int.Parse(commands[2]);
+
+                if (row < 0 || row > arrays.Length - 1 ||
+                    col < 0 || col > arrays[row].Length - 1)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                    input = Console.ReadLine();
+                    continue;
+                }
+                string action = commands[0];
+                int value = int.Parse(commands[3]);
+
+                if (action == "Add")
+                {
+                    arrays[row][col] += value;
+                }
+                else if (action == "Subtract")
+                {
+                    arrays[row][col] -= value;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (int[] array in arrays)
+            {
+                Console.WriteLine(string.Join(' ', array));
+            }
         }
     }
 }
--- Advanced/02.Multidimentional Arrays/Multidimentional Arrays-Lab/T07. Pascal Triangle/Program.cs
+++ Advanced/02.Multidimentional Arrays/Multidimentional Arrays-Lab/T07. Pascal Triangle/Program.cs
@@ -7,7 +7,26 @@ namespace T07._Pascal_Triangle
     {
         static void Main(string[] args)
         {
-            
+            int rows = int.Parse(Console.ReadLine());
+
+            long[][] pascalTriangle = new long[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                pascalTriangle[i] = new long[i + 1];
+                pascalTriangle[i][0] = 1;
+                pascalTriangle[i][pascalTriangle[i].Length-1] = 1;
+
+                for (int j = 1; j < pascalTriangle[i].Length-1; j++)
+                {
+                    pascalTriangle[i][j] = pascalTriangle[i-1][j-1] + pascalTriangle[i-1][j];
+                }
+            }
+
+            foreach (var row in pascalTriangle)
+            {
+                Console.WriteLine(string.Join(' ', row));
+            }
         }
     }
 }
