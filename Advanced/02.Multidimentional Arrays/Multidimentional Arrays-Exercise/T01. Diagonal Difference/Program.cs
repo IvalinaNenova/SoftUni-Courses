972bdaa6a3a930d761aee623d08883a6320600b0
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-11 15:16:54 +0300

    Created solution for Multidimentional Arrays - Exercise

---
+++ Advanced/02.Multidimentional Arrays/Multidimentional Arrays-Exercise/T01. Diagonal Difference/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T01._Diagonal_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
