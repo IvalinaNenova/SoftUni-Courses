2f7c0d47ec4874a1589229a1776e549a079b6fb2
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-19 17:19:38 +0300

    Added solution to task 05

--- Advanced/02.Multidimentional Arrays/Multidimentional Arrays-Exercise/T05. Snake Moves/Program.cs
+++ Advanced/02.Multidimentional Arrays/Multidimentional Arrays-Exercise/T05. Snake Moves/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
 
 namespace T05._Snake_Moves
 {
@@ -6,7 +9,43 @@ namespace T05._Snake_Moves
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] matrixDimensions = Console.ReadLine().Split(' ');
+            int rows = int.Parse(matrixDimensions[0]);
+            int columns = int.Parse(matrixDimensions[1]);
+
+            string snake = Console.ReadLine();
+            Queue<char> charQueue = new Queue<char>(snake);
+            char[,] matrix = new char[rows, columns];
+
+            for (int row = 0; row < rows; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    for (int col = 0; col < columns; col++)
+                    {
+                        matrix[row, col] = charQueue.Peek();
+                        charQueue.Enqueue(charQueue.Dequeue());
+                    }
+                }
+                else
+                {
+                    for (int col = columns-1; col >= 0; col--)
+                    {
+                        matrix[row, col] = charQueue.Peek();
+                        charQueue.Enqueue(charQueue.Dequeue());
+                    }
+                }
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matrix[i,j]);
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
