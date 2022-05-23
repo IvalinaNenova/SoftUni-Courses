edc25c4e9a3be071f71bac2d0e0689f85f9aad06
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-23 14:34:15 +0300

    Created blank console App for task 01

---
+++ Advanced/03.Sets and Dictionaries Advanced/Sets and Dictionaries Advanced - Lab/T01. Count Same Values in Array/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T01._Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
