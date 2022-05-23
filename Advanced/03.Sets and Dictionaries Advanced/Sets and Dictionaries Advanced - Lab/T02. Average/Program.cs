34f38d2680a4368f0cf5ca5570799c54536cb79c
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-23 14:40:38 +0300

    deleted folder

---
+++ Advanced/03.Sets and Dictionaries Advanced/Sets and Dictionaries Advanced - Lab/T02. Average/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T02._Average
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
