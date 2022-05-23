cdaca105d5990941d84896b6685164757c1da6d2
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-23 16:56:23 +0300

    Completed task 05

--- Advanced/03.Sets and Dictionaries/Sets and Dictionaries - Lab/T05. Cities by Continent and Country/Cities by Continent and Country.cs
+++ Advanced/03.Sets and Dictionaries/Sets and Dictionaries - Lab/T05. Cities by Continent and Country/Cities by Continent and Country.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace T05._Cities_by_Continent_and_Country
 {
@@ -6,7 +7,39 @@ namespace T05._Cities_by_Continent_and_Country
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, List<string>>> cities =
+                new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine().Split(' ');
+                string continent = data[0];
+                string country = data[1];
+                string city = data[2];
+
+                if (!cities.ContainsKey(continent))
+                {
+                    cities[continent] = new Dictionary<string, List<string>>();
+                }
+
+                if (!cities[continent].ContainsKey(country))
+                {
+                    cities[continent][country]= new List<string>();
+                }
+
+                cities[continent][country].Add(city);
+            }
+
+            foreach (var continent in cities)
+            {
+                Console.WriteLine(continent.Key + ":");
+
+                foreach (var country in continent.Value)
+                {
+                    Console.WriteLine($"{country.Key} -> {string.Join(", ", country.Value)}");
+                }
+            }
         }
     }
 }
