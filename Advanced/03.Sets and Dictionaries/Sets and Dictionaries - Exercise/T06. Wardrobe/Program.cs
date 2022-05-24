c13f4d25276909a61804da4bbd537d771f3fa452
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-24 17:09:45 +0300

    Uploaded solution to Task 06

--- Advanced/03.Sets and Dictionaries/Sets and Dictionaries - Exercise/T06. Wardrobe/Program.cs
+++ Advanced/03.Sets and Dictionaries/Sets and Dictionaries - Exercise/T06. Wardrobe/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace T06._Wardrobe
 {
@@ -6,7 +7,50 @@ namespace T06._Wardrobe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberOfColors = int.Parse(Console.ReadLine());
+            var wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+            for (int i = 0; i < numberOfColors; i++)
+            {
+                string[] wardrobeData = Console.ReadLine().Split(" -> ");
+                string color = wardrobeData[0];
+                string[] itemOfClothing = wardrobeData[1].Split(',');
+
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe[color] = new Dictionary<string, int>();
+                }
+
+                foreach (var item in itemOfClothing)
+                {
+                    if (!wardrobe[color].ContainsKey(item))
+                    {
+                        wardrobe[color][item] = 0;
+                    }
+
+                    wardrobe[color][item]++;
+                }
+            }
+
+            string[] itemToLookFor = Console.ReadLine().Split(' ');
+            string searchedColor = itemToLookFor[0];
+            string searchedItem = itemToLookFor[1];
+
+            foreach (var (color, item) in wardrobe)
+            {
+                Console.WriteLine($"{color} clothes:");
+                foreach (var (piece, count) in item)
+                {
+                    if (color == searchedColor && piece == searchedItem)
+                    {
+                        Console.WriteLine($"* {piece} - {count} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {piece} - {count}");
+                    }
+                }
+            }
         }
     }
 }
