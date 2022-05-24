5a007b643d82b9cab6217ad4630282351df05103
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-24 16:37:38 +0300

    Added solution to Task 04

--- Advanced/03.Sets and Dictionaries/Sets and Dictionaries - Exercise/T04. Even Times/Program.cs
+++ Advanced/03.Sets and Dictionaries/Sets and Dictionaries - Exercise/T04. Even Times/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace T04._Even_Times
 {
@@ -6,7 +8,23 @@ namespace T04._Even_Times
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            var numbers = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (!numbers.ContainsKey(number))
+                {
+                    numbers[number] = 0;
+                }
+                numbers[number]++;
+            }
+
+            int num = numbers.First(key => key.Value % 2 == 0).Key;
+
+            Console.WriteLine(num);
         }
     }
 }
