51f0153cf87186cbd2a1f75371546ae023aea748
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-24 16:23:23 +0300

    Added solution to task 02

--- Advanced/03.Sets and Dictionaries/Sets and Dictionaries - Exercise/T02. Sets of Elements/Program.cs
+++ Advanced/03.Sets and Dictionaries/Sets and Dictionaries - Exercise/T02. Sets of Elements/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace T02._Sets_of_Elements
 {
@@ -6,7 +8,26 @@ namespace T02._Sets_of_Elements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] counts = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = counts[0];
+            int m = counts[1];
+
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                firstSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                secondSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            var list = firstSet.Intersect(secondSet);
+
+            Console.WriteLine(string.Join(" ", list));
         }
     }
 }
