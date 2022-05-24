0ce0b2a973208e8f4be9303bef9cf7f70ee55456
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-24 16:49:10 +0300

    Added solution to Task 05

--- Advanced/03.Sets and Dictionaries/Sets and Dictionaries - Exercise/T05. Count Symbols/Program.cs
+++ Advanced/03.Sets and Dictionaries/Sets and Dictionaries - Exercise/T05. Count Symbols/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace T05._Count_Symbols
 {
@@ -6,7 +7,25 @@ namespace T05._Count_Symbols
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+
+            SortedDictionary<char,int> symbols = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char symbol = input[i];
+
+                if (!symbols.ContainsKey(symbol))
+                {
+                    symbols[symbol] = 0;
+                }
+                symbols[symbol]++;
+            }
+
+            foreach (var (symbol, count) in symbols)
+            {
+                Console.WriteLine($"{symbol}: {count} time/s");
+            }
         }
     }
 }
