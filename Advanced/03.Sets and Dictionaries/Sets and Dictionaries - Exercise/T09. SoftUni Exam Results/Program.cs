e1ec7fb235509732cd0804688488a6bc2d9c09eb
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-25 16:07:54 +0300

    Added solution to task 09

--- Advanced/03.Sets and Dictionaries/Sets and Dictionaries - Exercise/T09. SoftUni Exam Results/Program.cs
+++ Advanced/03.Sets and Dictionaries/Sets and Dictionaries - Exercise/T09. SoftUni Exam Results/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace T09._SoftUni_Exam_Results
 {
@@ -6,7 +8,65 @@ namespace T09._SoftUni_Exam_Results
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var statistics = new SortedDictionary<string, int>();
+            var studentResults = new SortedDictionary<string, int>();
+
+            string input = Console.ReadLine();
+
+            while (input != "exam finished")
+            {
+                string[] data = input.Split('-');
+                string username = data[0];
+                string language = data[1];
+
+                if (language != "banned")
+                {
+                    int points = int.Parse(data[2]);
+                    if (!studentResults.ContainsKey(username))
+                    {
+                        studentResults[username] = points;
+
+                    }
+                    else if (studentResults[username] < points)
+                    {
+                        studentResults[username] = points;
+                    }
+
+                    if (!statistics.ContainsKey(language))
+                    {
+                        statistics[language] = 0;
+                    }
+
+                    statistics[language]++;
+
+                }
+                else
+                {
+                    if (studentResults.ContainsKey(username))
+                    {
+                        studentResults.Remove(username);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            var ordered = studentResults.OrderByDescending(user => user.Value);
+
+            Console.WriteLine("Results:");
+            if (ordered.Any())
+            {
+                foreach (var (student, points) in ordered)
+                {
+                    Console.WriteLine($"{student} | {points}");
+                }
+            }
+
+            Console.WriteLine("Submissions:");
+            foreach (var (language, entries) in statistics.OrderByDescending(x => x.Value))
+            {
+                Console.WriteLine($"{language} - {entries}");
+            }
         }
     }
 }
