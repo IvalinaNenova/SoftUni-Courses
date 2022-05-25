6e5185044474ea328c24b3862e0c3a623db99a16
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-25 17:33:33 +0300

    Added solution to Task 10

--- Advanced/03.Sets and Dictionaries/Sets and Dictionaries - Exercise/T10. ForceBook/Program.cs
+++ Advanced/03.Sets and Dictionaries/Sets and Dictionaries - Exercise/T10. ForceBook/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace T10._ForceBook
 {
@@ -6,7 +8,88 @@ namespace T10._ForceBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var users = new SortedDictionary<string, SortedSet<string>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "Lumpawaroo")
+            {
+                string[] command = input.Split(new string[] { " -> ", " | " }, StringSplitOptions.RemoveEmptyEntries);
+                string side = command[0];
+                string user = command[1];
+
+                if (input.Contains('|'))
+                {
+                    bool isFound = users.Values.Any(userData => userData.Contains(user));
+
+                    if (!isFound)
+                    {
+                        if (!users.ContainsKey(side))
+                        {
+                            users[side] = new SortedSet<string>();
+                        }
+
+                        users[side].Add(user);
+                    }
+                }
+
+                else if (input.Contains("->"))
+                {
+                    user = command[0];
+                    side = command[1];
+                    bool isFound = false;
+
+                    foreach (var userData in users)
+                    {
+                        foreach (var person in userData.Value)
+                        {
+                            if (person == user)
+                            {
+                                users[userData.Key].Remove(person);
+                                isFound = true;
+                                if (!users.ContainsKey(side))
+                                {
+                                    users[side] = new SortedSet<string>();
+                                    
+                                }
+                                users[side].Add(user);
+                                break;
+                            }
+                        }
+                        if (isFound)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (!isFound)
+                    {
+                        if (!users.ContainsKey(side))
+                        {
+                            users[side] = new SortedSet<string>();
+                        }
+                        users[side].Add(user);
+                    }
+
+                    Console.WriteLine($"{user} joins the {side} side!");
+                }
+
+                input = Console.ReadLine();
+            }
+
+            var ordered = users
+                .Where(x => x.Value.Any())
+                .OrderByDescending(y => y.Value.Count);
+
+            foreach (var (side, members) in ordered)
+            {
+                Console.WriteLine($"Side: {side}, Members: {members.Count}");
+
+                foreach (var member in members)
+                {
+                    Console.WriteLine($"! {member}");
+                }
+            }
         }
     }
 }
