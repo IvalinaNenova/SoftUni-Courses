d6f4e8b1f4bff75f88b9d510498fd5016b312842
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-27 14:54:46 +0300

    Added solution to task 01 OddLines

---
+++ Advanced/04.Streams, Files and Directories/Streams, Files and Directories - Lab/OddLines/OddLines.cs
@@ -0,0 +1,36 @@
+﻿
+namespace OddLines
+{
+    using System;
+    using System.IO;
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputPath, outputPath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+           using StreamReader reader = new StreamReader(inputFilePath);
+           using StreamWriter writer = new StreamWriter(outputFilePath);
+
+           int lineNumber = 0;
+
+           while (!reader.EndOfStream)
+           {
+               string line = reader.ReadLine();
+
+               if (lineNumber % 2 == 1)
+               {
+                   writer.WriteLine(line);
+               }
+
+               lineNumber++;
+           }
+        }
+    }
+}
