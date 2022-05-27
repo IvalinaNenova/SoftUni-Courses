276833b7d344567404f8141ae7fd67934e97e99c
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-27 16:28:33 +0300

    Added ssolution to task 04 Merge Files

---
+++ Advanced/04.Streams, Files and Directories/Streams, Files and Directories - Lab/MergeFiles/MergeFiles.cs
@@ -0,0 +1,30 @@
+﻿namespace MergeFiles
+{
+    using System;
+    using System.IO;
+    public class MergeFiles
+    {
+        static void Main(string[] args)
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+            using StreamReader firstReader = new StreamReader(firstInputFilePath);
+            using StreamReader secondReader = new StreamReader(secondInputFilePath);
+            using StreamWriter writer = new StreamWriter(outputFilePath);
+
+            while (!firstReader.EndOfStream)
+            {
+                writer.WriteLine(firstReader.ReadLine());
+                writer.WriteLine(secondReader.ReadLine());
+            }
+            writer.WriteLine(secondReader.ReadLine());
+        }
+    }
+}
