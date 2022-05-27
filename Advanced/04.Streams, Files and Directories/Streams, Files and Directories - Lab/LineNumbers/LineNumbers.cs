a38dd45ea786771b7bbc49469bfdf31d4e0b56d8
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-27 16:27:14 +0300

    Added solution to task 02 LineNumbers

---
+++ Advanced/04.Streams, Files and Directories/Streams, Files and Directories - Lab/LineNumbers/LineNumbers.cs
@@ -0,0 +1,29 @@
+﻿namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            using StreamReader sr = new StreamReader(inputFilePath);
+            using StreamWriter sw = new StreamWriter(outputFilePath);
+
+            int rowNumber = 1;
+
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                sw.WriteLine($"{rowNumber}. {line}");
+                rowNumber++;
+            }
+        }
+    }
+}
