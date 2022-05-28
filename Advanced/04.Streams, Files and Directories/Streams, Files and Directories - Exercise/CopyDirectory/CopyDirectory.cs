aaa74d2b53723491d68e5612d90414d15e7c039e
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-28 12:25:00 +0300

    Added solution to Task 05

---
+++ Advanced/04.Streams, Files and Directories/Streams, Files and Directories - Exercise/CopyDirectory/CopyDirectory.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+namespace CopyDirectory
+{
+    using System;
+    public class CopyDirectory
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = Console.ReadLine();
+            string outputPath = Console.ReadLine();
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            if (Directory.Exists(outputPath))
+            {
+                Directory.Delete(outputPath);
+            }
+            Directory.CreateDirectory(outputPath);
+
+            var files = Directory.GetFiles(inputPath);
+
+            foreach (var file in files)
+            {
+                var fileName = Path.GetFileName(file);
+                var copyDestination = Path.Combine(outputPath, fileName);
+                File.Copy(fileName, copyDestination);
+            }
+        }
+    }
+}
