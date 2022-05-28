47c111259ecad76351427a41a6aa99c07ed4d179
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-28 10:21:11 +0300

    Added solution to last task

---
+++ Advanced/04.Streams, Files and Directories/Streams, Files and Directories - Lab/FolderSize/FolderSize.cs
@@ -0,0 +1,20 @@
+﻿namespace FolderSize
+{
+    using System;
+    using System.IO;
+    public class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+
+        }
+    }
+}
