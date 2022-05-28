6d4110f7481fdc0357bf5c2a8f84d816999d7970
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-28 11:07:20 +0300

    Added solution to task 02 Line Numbers

---
+++ Advanced/04.Streams, Files and Directories/Streams, Files and Directories - Exercise/LineNumbers/LineNumbers.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+
+namespace LineNumbers
+{
+    using System;
+    public class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"..\..\..\text.txt";
+            string outputPath = @"..\..\..\output.txt";
+
+            ProcessLines(inputPath, outputPath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            using StreamReader reader = new StreamReader(inputFilePath);
+            using StreamWriter writer = new StreamWriter(outputFilePath);
+
+            int rowNumber = 1;
+            while (!reader.EndOfStream)
+            {
+                int countLetters = 0;
+                int countNonLetters = 0;
+                string line = reader.ReadLine();
+                foreach (var symbol in line)
+                {
+                    if (char.IsLetter(symbol))
+                    {
+                        countLetters++;
+                    }
+
+                    if (!char.IsLetterOrDigit(symbol) && symbol != ' ')
+                    {
+                        countNonLetters++;
+                    }
+                }
+
+                rowNumber++;
+
+                writer.WriteLine($"Line {rowNumber}: {line} ({countLetters})({countNonLetters})");
+            }
+        }
+    }
+}
