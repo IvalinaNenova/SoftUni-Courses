ff02491fa726d4e0f04e3d2cdac9ab4ddf5735c7
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-28 13:48:26 +0300

    Updated solution to task 03

---
+++ Advanced/04.Streams, Files and Directories/Streams, Files and Directories - Exercise/CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace CopyBinaryFile
+{
+    public class CopyBinaryFile
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"..\..\..\copyMe.png";
+            string outputPath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputPath, outputPath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using FileStream reader = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read);
+            using FileStream writer = new FileStream(outputFilePath, FileMode.Create, FileAccess.Write);
+
+            while (true)
+            {
+                byte[] buffer = new byte[4096];
+                int bytesRead = reader.Read(buffer, 0, buffer.Length);
+                if (bytesRead == 0)
+                {
+                    break;
+                }
+                writer.Write(buffer, 0, bytesRead);
+            }
+        }
+    }
+}
