a8c32cf756f4b7c7c953971af84abc930e4d4560
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-28 10:51:10 +0300

    Added solution to task 01

---
+++ Advanced/04.Streams, Files and Directories/Streams, Files and Directories - Exercise/EvenLines/EvenLines.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace EvenLines
+{
+    using System;
+    public class EvenLines
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            using StreamReader reader = new StreamReader(inputFilePath);
+            int rowNumber = 0;
+            StringBuilder sb = new StringBuilder();
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (rowNumber % 2 == 0)
+                {
+                    string replaced = ReplaceSymbols(line);
+                    string processedLine = ReverseWords(replaced);
+                    sb.AppendLine(processedLine);
+                }
+
+                rowNumber++;
+            }
+
+            return sb.ToString();
+        }
+        private static string ReverseWords(string replacedSymbols)
+        {
+            return string.Join(" ", replacedSymbols.Split(' ').Reverse());
+        }
+
+        private static string ReplaceSymbols(string line)
+        {
+            return line.Replace('-', '@')
+                .Replace(',', '@')
+                .Replace('.', '@')
+                .Replace('?', '@')
+                .Replace('!', '@');
+        }
+    }
+
+}
