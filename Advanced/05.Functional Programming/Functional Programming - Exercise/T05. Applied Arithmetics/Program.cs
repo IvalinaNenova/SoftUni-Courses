6c433a80086d3fa6144a138643cb27531fb81730
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-30 21:00:46 +0300

    Updated code to task 05

--- Advanced/05.Functional Programming/Functional Programming - Exercise/T05. Applied Arithmetics/Program.cs
+++ Advanced/05.Functional Programming/Functional Programming - Exercise/T05. Applied Arithmetics/Program.cs
@@ -14,36 +14,43 @@ namespace T05._Applied_Arithmetics
                 .Select(int.Parse)
                 .ToArray();
             string command = Console.ReadLine();
-            Func<int, int> function = num => num;
             Action<int[]> print = numbers => Console.WriteLine(string.Join(" ", numbers));
 
             while (command != "end")
             {
-                if (command == "add")
-                {
-                    function = n => n + 1;
-                    numbers = numbers.Select(function).ToArray();
 
-                }
-                else if (command == "multiply")
-                {
-                    function = n => n * 2;
-                    numbers = numbers.Select(function).ToArray();
-
-                }
-                else if (command == "subtract")
-                {
-                    function = n => n - 1;
-                    numbers = numbers.Select(function).ToArray();
-
-                }
-                else if (command == "print")
+                if (command == "print")
                 {
                     print(numbers);
+                }
+                else
+                {
+                    Func<int, int> function = GetFunction(command);
+                    numbers = numbers.Select(function).ToArray();
                 }
 
                 command = Console.ReadLine();
             }
+        }
+
+        public static Func<int, int> GetFunction(string command)
+        {
+            Func<int, int> function = n => n;
+
+            if (command == "add")
+            {
+                function = n => n + 1;
+            }
+            else if (command == "multiply")
+            {
+                function = n => n * 2;
+            }
+            else if (command == "subtract")
+            {
+                function = n => n - 1;
+            }
+
+            return function;
         }
     }
 }
