a0fa44c6e1c36a34516fd05252f9fff291aa4f3a
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-30 20:08:52 +0300

    Completed Task 04 Find Evens or Odds

---
+++ Advanced/05.Functional Programming/Functional Programming - Exercise/T04. Find Evens or Odds/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _4.Find_Evens_or_Odds
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class FindEvensOrOdds
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var start = input[0];
+            var end = input[1];
+
+            var targetNumbers = Console.ReadLine().Trim().ToLower();
+            Predicate<int> predicate;
+
+            switch (targetNumbers)
+            {
+                case "odd":
+                    predicate = n => n % 2 != 0;
+                    break;
+                case "even":
+                    predicate = n => n % 2 == 0;
+                    break;
+                default:
+                    predicate = null;
+                    break;
+            }
+
+            var result = EvensOrOdds(start, end, predicate);
+            Console.WriteLine(string.Join(" ", result));
+
+        }
+
+        public static List<int> EvensOrOdds(int lowerBound, int upperBound, Predicate<int> filter)
+        {
+            var numbers = new List<int>();
+
+            for (int i = lowerBound; i <= upperBound; i++)
+            {
+                if (filter(i))
+                {
+                    numbers.Add(i);
+                }
+            }
+
+            return numbers;
+        }
+    }
+}
