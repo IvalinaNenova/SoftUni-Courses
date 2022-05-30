675cd46eb6f86d68326f5ad77f32fffb82bccb3d
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-30 22:07:06 +0300

    Completed Task 08. List of Predicates

---
+++ Advanced/05.Functional Programming/Functional Programming - Exercise/T08. List Of Predicates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace T08._List_Of_Predicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rangeEnd = int.Parse(Console.ReadLine());
+            List<int> numbers = Enumerable.Range(1, rangeEnd).ToList();
+            List<int> dividers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            foreach (var divider in dividers)
+            {
+                Func<int, bool> predicate = num => num % divider == 0;
+
+                numbers = numbers.Where(predicate).ToList();
+
+            }
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
