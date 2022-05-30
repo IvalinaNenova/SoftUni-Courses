1f8659c1629dc2afa352f908227702d0fe335305
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-30 14:47:30 +0300

    Completed Task 03 Count Uppercase Words

---
+++ Advanced/05.Functional Programming/Functional Programming - Lab/T03. Count Uppercase Words/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace T03._Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(word => char.IsUpper(word[0]))
+                .ToArray();
+
+            foreach (string word in text)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
