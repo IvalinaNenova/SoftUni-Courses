9eeee25001efc8777fb03b041d5c3169c8de41e1
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-30 21:40:47 +0300

    Completed Task 07. Predicate for names

---
+++ Advanced/05.Functional Programming/Functional Programming - Exercise/T07. Predicate For Names/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T07._Predicate_For_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int targetLength = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split(' ').ToList();
+
+            Func<string, bool> function = name => name.Length <= targetLength;
+
+            names = names.Where(function).ToList();
+
+            Action<List<string>> print = names => Console.WriteLine(string.Join(Environment.NewLine, names));
+
+            print(names);
+        }
+    }
+}
