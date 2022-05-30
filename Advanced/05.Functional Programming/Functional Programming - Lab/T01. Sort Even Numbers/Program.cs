3d4703a807137141b1cf9c840068cd097cc708e4
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-30 14:37:22 +0300

    Created folder for Functional Programming - Lab

---
+++ Advanced/05.Functional Programming/Functional Programming - Lab/T01. Sort Even Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace T01._Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var evenNumbers = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .Where(num => num % 2 == 0)
+                .OrderBy(x => x);
+            Console.WriteLine(string.Join(", ", evenNumbers));
+        }
+    }
+}
---
+++ Advanced/05.Functional Programming/Functional Programming - Lab/Т02. Sum Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Т02._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
