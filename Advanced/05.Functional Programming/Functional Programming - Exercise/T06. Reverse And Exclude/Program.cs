ed1d406be330efb8f91dacf43427f16de608d0a3
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-30 21:29:35 +0300

    Completed task 06. Reverse and exclude

---
+++ Advanced/05.Functional Programming/Functional Programming - Exercise/T06. Reverse And Exclude/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T06._Reverse_And_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+           
+            int divider = int.Parse(Console.ReadLine());
+
+            Predicate<int> filter = num => num % divider != 0;
+
+            numbers.Reverse();
+            numbers = MyWhere(numbers, filter);
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+
+        static List<int> MyWhere(List<int> numbers, Predicate<int> filter)
+        {
+            List<int> newList = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (filter(number))
+                {
+                    newList.Add(number);
+                }
+            }
+
+            return newList;
+        }
+    }
+}
