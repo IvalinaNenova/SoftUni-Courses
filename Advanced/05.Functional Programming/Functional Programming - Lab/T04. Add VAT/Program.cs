e52b0ff3911937a527a2e18e51728c9a1712478e
Ivalina Nenova <nenovaivalina@gmail.com>
2022-05-30 14:53:05 +0300

    Completed Task 04 Add VAT

---
+++ Advanced/05.Functional Programming/Functional Programming - Lab/T04. Add VAT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace T04._Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double> addVatFunc = price => price * 1.2;
+            var prices = Console.ReadLine()
+                .Split(", ")
+                .Select(double.Parse)
+                .Select(addVatFunc)
+                .ToArray();
+
+            foreach (var price in prices)
+            {
+                Console.WriteLine($"{price:f2}");
+            }
+        }
+    }
+}
