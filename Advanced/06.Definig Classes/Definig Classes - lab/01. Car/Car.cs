9122c6f5b738c62e8d6e8c24dd594cfb54d5cf32
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-01 20:06:46 +0300

    Created folder for Defining Classes - Lab

---
+++ Advanced/06.Definig Classes/Definig Classes - lab/01. Car/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01._Car
+{
+    internal class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        
+    }
+}
---
+++ Advanced/06.Definig Classes/Definig Classes - lab/01. Car/CarManufacturer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using _01._Car;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "WV";
+            car.Model = "MK3";
+            car.Year = 1992;
+
+            Console.WriteLine($"Make {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+
+        }
+    }
+}
