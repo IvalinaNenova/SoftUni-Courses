b2452528b9eb0ce7a8d0c65145f3fc04ba11eee6
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-01 11:26:57 +0300

    Updated code to Task 10. Radioactive Mutant Vampire Bunnies

--- Advanced/02.Multidimentional Arrays/Multidimentional Arrays-Exercise/T10. Radioactive Mutant Vampire/Program.cs
+++ Advanced/02.Multidimentional Arrays/Multidimentional Arrays-Exercise/T10. Radioactive Mutant Vampire/Program.cs
@@ -8,96 +8,81 @@ namespace T10._Radioactive_Mutant_Vampire
 {
     internal class Program
     {
+        public
         static void Main(string[] args)
         {
-            string[] dimensions = Console.ReadLine().Split(' ');
-            int rows = int.Parse(dimensions[0]);
-            int columns = int.Parse(dimensions[1]);
+            string[] dimensionsAsString = Console.ReadLine().Split(' ');
+            int rows = int.Parse(dimensionsAsString[0]);
+            int cols = int.Parse(dimensionsAsString[1]);
+            char[,] field = new char[rows, cols];
 
-            char[,] field = new char[rows, columns];
+            var playerPosition = FillAndReturnPlayerPosition(field);
+            HashSet<(int, int)> bunnyPositions = GetBunnyPositions(field);
+            string directions = Console.ReadLine();
+            bool playerWon = false;
+            bool bunniesWon = false;
+            
 
-            int[] playerPosition = FillAndReturnPlayerPosition(rows, columns, field);
-            int playerRow = playerPosition[0];
-            int playerColumn = playerPosition[1];
-            int[] newPosition = new int[2];
-
-
-            string moves = Console.ReadLine();
-            bool isDead = false;
-            bool hasWon = false;
-            bool isKilledByBunny = false;
-
-
-            while (!hasWon && !isDead && !isKilledByBunny)
+            while (!playerWon && !bunniesWon)
             {
-                foreach (char move in moves)
+                foreach (var direction in directions)
                 {
-                    field[playerRow, playerColumn] = '.';
-                    newPosition = MovePlayer(playerRow, playerColumn, move);
-                    int newRow = newPosition[0];
-                    int newCol = newPosition[1];
+                    var newPosition = MovePlayer(playerPosition, direction);
+                    field[playerPosition.Item1, playerPosition.Item2] = '.';
 
-                    if (!CheckPosition(newRow, newCol, rows, columns))
+                    if (bunnyPositions.Contains(newPosition))
                     {
-                        hasWon = true;
+                        bunniesWon = true;
+                        playerPosition = newPosition;
+
                     }
-                    else if (field[newRow,newCol] == 'B')
+                    else if (!CheckPosition(newPosition.Item1, newPosition.Item2, field))
                     {
-                        isDead = true;
+                        playerWon = true;
                     }
-
-                    if (!isDead && !hasWon)
+                    else
                     {
-                        playerRow = newPosition[0];
-                        playerColumn = newPosition[1];
-                        field[playerRow, playerColumn] = 'P';
+                        field[newPosition.Item1, newPosition.Item2] = 'P';
+                        playerPosition = newPosition;
                     }
+                   
+                    bunnyPositions.UnionWith(SpreadBunnies(field, bunnyPositions));
 
-                    Queue<int> bunnyPositions = GetBunnyPositions(rows, columns, field);
-
-                    while (bunnyPositions.Count > 0)
+                    if (bunnyPositions.Contains(newPosition) && !playerWon)
                     {
-                        int row = bunnyPositions.Dequeue();
-                        int col = bunnyPositions.Dequeue();
+                        bunniesWon = true;
 
-                        isKilledByBunny = SpreadBunnies(field, row, col, hasWon);
                     }
 
-                    if (isDead || hasWon || isKilledByBunny)
+                    if (playerWon || bunniesWon)
                     {
                         break;
                     }
                 }
             }
 
-            if (hasWon)
+            if (playerWon)
             {
                 PrintMatrix(field);
-                Console.WriteLine($"won: {playerRow} {playerColumn}");
+                Console.WriteLine($"won: {playerPosition.Item1} {playerPosition.Item2}");
             }
-            else if (isDead)
+            else
             {
                 PrintMatrix(field);
-                Console.WriteLine($"dead: {newPosition[0]} {newPosition[1]}");
-            }
-            else if (isKilledByBunny)
-            {
-                PrintMatrix(field);
-                Console.WriteLine($"dead: {newPosition[0]} {newPosition[1]}");
+                Console.WriteLine($"dead: {playerPosition.Item1} {playerPosition.Item2}");
             }
         }
 
-        private static Queue<int> GetBunnyPositions(int rows, int columns, char[,] field)
+        private static HashSet<(int, int)> GetBunnyPositions(char[,] field)
         {
-            Queue<int> bunnyPositions = new Queue<int>();
-            for (int row = 0; row < rows; row++)
+            HashSet<(int, int)> bunnyPositions = new HashSet<(int, int)>();
+            for (int row = 0; row < field.GetLength(0); row++)
             {
-                for (int col = 0; col < columns; col++)
+                for (int col = 0; col < field.GetLength(1); col++)
                 {
                     if (field[row, col] == 'B')
                     {
-                        bunnyPositions.Enqueue(row);
-                        bunnyPositions.Enqueue(col);
+                        bunnyPositions.Add((row, col));
                     }
                 }
             }
@@ -105,56 +90,49 @@ namespace T10._Radioactive_Mutant_Vampire
             return bunnyPositions;
         }
 
-        private static bool SpreadBunnies(char[,] field, int row, int col, bool hasWon)
+        private static HashSet<(int, int)> SpreadBunnies(char[,] field, HashSet<(int, int)> bunnyPositions)
         {
-            int maxRowLength = field.GetLength(0);
-            int maxColLength = field.GetLength(1);
-            bool isOver = false;
+            HashSet<(int, int)> newPositions = new HashSet<(int, int)>();
+            foreach (var bunnyPosition in bunnyPositions)
+            {
+                int row = bunnyPosition.Item1;
+                int col = bunnyPosition.Item2;
 
-            if (CheckPosition(row - 1, col, maxRowLength, maxColLength ))
-            {
-                if (field[row, col] == 'P' && !hasWon)
+                if (CheckPosition(row - 1, col, field))
                 {
-                    isOver = true;
+                    field[row - 1, col] = 'B';
+                    newPositions.Add((row - 1, col));
                 }
-                field[row - 1, col] = 'B';
-            }
-            if (CheckPosition(row + 1, col, maxRowLength, maxColLength))
-            {
-                if (field[row, col] == 'P' && !hasWon)
+                if (CheckPosition(row + 1, col, field))
                 {
-                    isOver = true;
+                    field[row + 1, col] = 'B';
+                    newPositions.Add((row + 1, col));
                 }
-                field[row + 1, col] = 'B';
-            }
-            if (CheckPosition(row, col + 1, maxRowLength, maxColLength))
-            {
-                if (field[row, col] == 'P' && !hasWon)
+                if (CheckPosition(row, col + 1, field))
                 {
-                    isOver = true;
+                    field[row, col + 1] = 'B';
+                    newPositions.Add((row, col + 1));
                 }
-                field[row, col + 1] = 'B';
-            }
-            if (CheckPosition(row, col - 1, maxRowLength, maxColLength))
-            {
-                if (field[row, col] == 'P' && !hasWon)
+                if (CheckPosition(row, col - 1, field))
                 {
-                    isOver = true;
+                    field[row, col - 1] = 'B';
+                    newPositions.Add((row, col - 1));
                 }
-                field[row, col - 1] = 'B';
             }
 
-            return isOver;
+            return newPositions;
         }
-        private static bool CheckPosition(int newRow, int newCol, int rows, int cols)
+        private static bool CheckPosition(int newRow, int newCol, char[,] field)
         {
             return newRow >= 0 &&
                    newCol >= 0 &&
-                   newRow < rows &&
-                   newCol < cols;
+                   newRow < field.GetLength(0) &&
+                   newCol < field.GetLength(1);
         }
-        private static int[] MovePlayer(int row, int column, char move)
+        private static (int, int) MovePlayer((int, int) playerPosition, char move)
         {
+            var row = playerPosition.Item1;
+            var column = playerPosition.Item2;
             if (move == 'U')
             {
                 row--;
@@ -171,26 +149,25 @@ namespace T10._Radioactive_Mutant_Vampire
             {
                 column++;
             }
-            int[] newPosition = new int[] { row, column };
 
-            return newPosition;
+            return (row, column);
         }
 
-        private static int[] FillAndReturnPlayerPosition(int rows, int cols, char[,] field)
+        private static (int, int) FillAndReturnPlayerPosition(char[,] field)
         {
-            int[] playerPosition = new int[2];
+            var playerPosition = (0, 0);
 
-            for (int row = 0; row < rows; row++)
+            for (int row = 0; row < field.GetLength(0); row++)
             {
                 string matrixLine = Console.ReadLine();
 
-                for (int col = 0; col < cols; col++)
+                for (int col = 0; col < field.GetLength(1); col++)
                 {
                     field[row, col] = matrixLine[col];
+
                     if (field[row, col] == 'P')
                     {
-                        playerPosition[0] = row;
-                        playerPosition[1] = col;
+                        playerPosition = (row, col);
                     }
                 }
             }
