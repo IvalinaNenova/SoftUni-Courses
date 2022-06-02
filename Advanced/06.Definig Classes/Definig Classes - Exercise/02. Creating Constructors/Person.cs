3cef6c2efc1b54a4605890b1e18ce5f8db7a419a
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-02 19:07:50 +0300

    Added solution to Task 02. Creating Constructors

---
+++ Advanced/06.Definig Classes/Definig Classes - Exercise/02. Creating Constructors/Person.cs
@@ -0,0 +1,37 @@
+﻿namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get => this.name;
+            set => this.name = value;
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+
+        public Person(int age) : this()
+        {
+            Age = age;
+        }
+
+        public Person(string name, int age) : this(age)
+        {
+            Name =name;
+            Age = age;
+        }
+    }
+}
