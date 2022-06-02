acdf6f0d0b89612e6167d26f142710b877e5874b
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-02 18:55:45 +0300

    Created Folder for Defining Classes - Exercise

---
+++ Advanced/06.Definig Classes/Definig Classes - Exercise/01. Define a Class Person/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get => name;
+            set { this.name = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+    }
+}
---
+++ Advanced/06.Definig Classes/Definig Classes - Exercise/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person()
+            {
+                Name = "Peter",
+                Age = 20
+            };
+
+            Console.WriteLine(person1.Name+ " " +person1.Age);
+        }
+    }
+
+   
+}
