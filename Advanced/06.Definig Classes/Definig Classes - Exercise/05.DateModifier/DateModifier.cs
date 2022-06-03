ed3fa2751efbd44d2e1721ce8c6eaf93b583eca6
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-03 11:23:18 +0300

    Completed Task 05.DateModifier

---
+++ Advanced/06.Definig Classes/Definig Classes - Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class DateModifier
+    {
+        private TimeSpan _difference = new TimeSpan();
+
+        public TimeSpan GetDifference(string date1, string date2)
+        {
+            DateTime firstDate = DateTime.Parse(date1);
+            DateTime secondDate = DateTime.Parse(date2);
+            _difference = firstDate - secondDate;
+
+           return _difference.Duration();
+        }
+    }
+}
---
+++ Advanced/06.Definig Classes/Definig Classes - Exercise/05.DateModifier/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string date1 = Console.ReadLine();
+            string date2 = Console.ReadLine();
+            DateModifier difference = new DateModifier();
+            TimeSpan diff = difference.GetDifference(date1, date2);
+            Console.WriteLine(diff.Days.ToString());
+        }
+    }
+}
