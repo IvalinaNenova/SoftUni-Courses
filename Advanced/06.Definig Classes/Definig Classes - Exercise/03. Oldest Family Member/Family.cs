d73e0ae8a9cfee0e8656ba1fa9569822fa7577fc
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-03 10:22:56 +0300

    Completed to Task 03. Oldest family Member

---
+++ Advanced/06.Definig Classes/Definig Classes - Exercise/03. Oldest Family Member/Family.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+       private List<Person> _familyMembers = new List<Person>();
+
+        public void AddMembers(Person member)
+        {
+            _familyMembers.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return _familyMembers.OrderByDescending(person => person.Age).First();
+        }
+    }
+}
---
+++ Advanced/06.Definig Classes/Definig Classes - Exercise/03. Oldest Family Member/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            Family family = new Family();
+
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                string[] personData = Console.ReadLine().Split(' ');
+                string name = personData[0];
+                int age = int.Parse(personData[1]);
+
+                Person person = new Person(name, age);
+                family.AddMembers(person);
+            }
+
+            var oldestMember = family.GetOldestMember();
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+}
