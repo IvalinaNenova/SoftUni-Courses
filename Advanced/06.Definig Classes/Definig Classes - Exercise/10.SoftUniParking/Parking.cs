eef97aed1b2bc2b5b9ee8b8a368dd770393ab36d
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-06 20:03:24 +0300

    Renamed some variables

--- Advanced/06.Definig Classes/Definig Classes - Exercise/10.SoftUniParking/Parking.cs
+++ Advanced/06.Definig Classes/Definig Classes - Exercise/10.SoftUniParking/Parking.cs
@@ -6,41 +6,39 @@ namespace SoftUniParking
 {
     public class Parking
     {
-        protected int _capacity;
-        private List<Car> _cars;
-
+        private readonly int capacity;
+        private readonly List<Car> cars = new List<Car>();
         public Parking(int capacity)
         {
-            _cars = new List<Car>();
-            this._capacity = capacity;
+            this.capacity = capacity;
         }
-        public int Count => _cars.Count();
+        public int Count => cars.Count();
 
         public string AddCar(Car car)
         {
-            if (_cars.Any(x => x.RegistrationNumber == car.RegistrationNumber))
+            if (cars.Any(x => x.RegistrationNumber == car.RegistrationNumber))
             {
                 return "Car with that registration number, already exists!";
             }
 
-            if (Count == _capacity)
+            if (Count == capacity)
             {
                 return "Parking is full!";
             }
 
-            _cars.Add(car);
+            cars.Add(car);
             return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
         }
 
         public string RemoveCar(string registrationNumber)
         {
-            if (!_cars.Any(car => car.RegistrationNumber == registrationNumber))
+            if (!cars.Any(car => car.RegistrationNumber == registrationNumber))
             {
                 return "Car with that registration number, doesn't exist!";
             }
 
-            Car carToRemove = _cars.Find(car => car.RegistrationNumber == registrationNumber);
-            _cars.Remove(carToRemove);
+            Car carToRemove = cars.Find(car => car.RegistrationNumber == registrationNumber);
+            cars.Remove(carToRemove);
             return $"Successfully removed {registrationNumber}";
         }
 
@@ -48,17 +46,17 @@ namespace SoftUniParking
         {
             foreach (var registrationNumber in registrationNumbers)
             {
-                var carToRemove = _cars.FirstOrDefault(car => car.RegistrationNumber == registrationNumber);
+                var carToRemove = cars.FirstOrDefault(car => car.RegistrationNumber == registrationNumber);
                 if (carToRemove != null)
                 {
-                    _cars.Remove(carToRemove);
+                    cars.Remove(carToRemove);
                 }
             }
         }
 
         public Car GetCar(string registrationNumber)
         {
-            return _cars.Find(car => car.RegistrationNumber == registrationNumber);
+            return cars.Find(car => car.RegistrationNumber == registrationNumber);
         }
     }
 }
