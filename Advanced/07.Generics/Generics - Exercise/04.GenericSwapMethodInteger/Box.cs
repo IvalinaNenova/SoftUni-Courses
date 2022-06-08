321c13430a9d10d48f25270ed290cf8a2e240cb3
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-08 16:50:11 +0300

    Completed Task 04. Generic Swap Method Integer

---
+++ Advanced/07.Generics/Generics - Exercise/04.GenericSwapMethodInteger/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.GenericSwapMethodInteger
+{
+    public class Box<T>
+    {
+        public T Value { get; set; }
+
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value.GetType()}: {Value}";
+        }
+    }
+}
---
+++ Advanced/07.Generics/Generics - Exercise/04.GenericSwapMethodInteger/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.GenericSwapMethodInteger
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Box<int>> boxes = new List<Box<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                boxes.Add(new Box<int>(num));
+            }
+            
+            Swap<Box<int>>(boxes);
+
+            foreach (var box in boxes)
+            {
+                Console.WriteLine(box.ToString());
+            }
+        }
+
+        private static void Swap<T>(List<Box<int>> boxes)
+        {
+            string[] indices = Console.ReadLine().Split();
+            int index1 = int.Parse(indices[0]);
+            int index2 = int.Parse(indices[1]);
+
+            (boxes[index1], boxes[index2]) = (boxes[index2], boxes[index1]);
+        }
+    }
+}
