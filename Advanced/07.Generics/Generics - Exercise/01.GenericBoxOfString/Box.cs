ccdf57218cab7cbd2812d1c9c5a283646b7e765b
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-08 16:06:20 +0300

    Completed Task 01.GenericBoxOfstring

---
+++ Advanced/07.Generics/Generics - Exercise/01.GenericBoxOfString/Box.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.GenericBoxOfString
+{
+    public class Box<T>
+    {
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Value.GetType()}: {Value}";
+        }
+    }
+}
---
+++ Advanced/07.Generics/Generics - Exercise/01.GenericBoxOfString/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01.GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string s = Console.ReadLine();
+                Box<string> box = new Box<string>();
+                box.Value = s;
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
