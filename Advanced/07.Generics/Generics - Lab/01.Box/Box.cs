9c82053fc18e4f5ae15a69ba58ae4baf560f9420
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-08 16:13:03 +0300

    Updated code to tas 01.BoxOfT

--- Advanced/07.Generics/Generics - Lab/01.Box/Box.cs
+++ Advanced/07.Generics/Generics - Lab/01.Box/Box.cs
@@ -7,9 +7,8 @@ namespace BoxOfT
     public class Box<T>
     {
         private List<T> internalList  = new List<T>();
-       
-        public int Count { get; }
-        private int count = 0;
+
+        public int Count => internalList.Count;
 
         public void Add(T element)
         {
@@ -24,3 +23,22 @@ namespace BoxOfT
         }
     }
 }
+//public class Box<T>
+//{
+//private List<T> internalList = new List<T>();
+
+//public void Add(T item)
+//{
+//    internalList.Add(item);
+//}
+
+//public T Remove()
+//{
+//    T removedItem = internalList[internalList.Count - 1];
+//    internalList.RemoveAt(internalList.Count - 1);
+//    return removedItem;
+//}
+
+//public int Count => internalList.Count;
+
+//}
--- Advanced/07.Generics/Generics - Lab/01.Box/StartUp.cs
+++ Advanced/07.Generics/Generics - Lab/01.Box/StartUp.cs
@@ -8,12 +8,8 @@ namespace BoxOfT
         {
             Box<int> box = new Box<int>();
             box.Add(1);
-            box.Add(2);
-            box.Add(3);
             Console.WriteLine(box.Remove());
-            box.Add(4);
-            box.Add(5);
-            Console.WriteLine(box.Remove());
+            Console.WriteLine(box.Count);
          
         }
     }
