19daf880e2f7d3e0421f675e730bd4fff22ec234
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-08 16:36:15 +0300

    Completed Task 03. Generic Swap Method String

---
+++ Advanced/07.Generics/Generics - Exercise/03.GenericSwapMethodString/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.GenericSwapMethodString
+{
+    public class Box<T>
+    {
+        public T Value { get; set; }
+
+        public Box(T value)
+        {
+            Value = value;
+        }
+        public override string ToString()
+        {
+            return $"{Value.GetType()}: {Value}";
+        }
+    }
+}
--- Advanced/07.Generics/Generics - Exercise/03.GenericSwapMethodString/Program.cs
+++ Advanced/07.Generics/Generics - Exercise/03.GenericSwapMethodString/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _03.GenericSwapMethodString
 {
@@ -6,7 +7,30 @@ namespace _03.GenericSwapMethodString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            List<Box<string>> boxes = new List<Box<string>>();
+            for (int i = 0; i < n; i++)
+            {
+                string value = Console.ReadLine();
+                var box = new Box<string>(value);
+                boxes.Add(box);
+            }
+
+            Swap(boxes);
+
+            foreach (var box in boxes)
+            {
+                Console.WriteLine(box);
+            }
+        }
+
+        private static void Swap<T>(List<Box<T>> boxes)
+        {
+            string[] swapIndexes = Console.ReadLine().Split();
+            int index1 = int.Parse(swapIndexes[0]);
+            int index2 = int.Parse(swapIndexes[1]);
+
+            (boxes[index1], boxes[index2]) = (boxes[index2], boxes[index1]);
         }
     }
 }
