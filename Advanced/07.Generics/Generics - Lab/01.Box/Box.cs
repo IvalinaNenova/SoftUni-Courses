7e75431503bd3700e4310c08c19dccf9a6e31289
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-08 15:44:22 +0300

    Completed Generics - Lab tasks

---
+++ Advanced/07.Generics/Generics - Lab/01.Box/Box.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> internalList  = new List<T>();
+       
+        public int Count { get; }
+        private int count = 0;
+
+        public void Add(T element)
+        {
+            this.internalList.Add(element);
+        }
+
+        public T Remove()
+        {
+            var element = internalList[internalList.Count -1];
+            internalList.RemoveAt(internalList.Count -1);
+            return element;
+        }
+    }
+}
---
+++ Advanced/07.Generics/Generics - Lab/GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+        public static T[] Create<T>(int lenght, T item)
+        {
+            T[] arr = new T[lenght];
+            for (int i = 0; i < lenght; i++)
+            {
+                arr[i] = item;
+            }
+            return arr;
+        }
+    }
+}
