f219621638baa239d680acea263db69f70039c5d
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-09 20:08:27 +0300

    Completed Task 01.Library

---
+++ Advanced/08.Iterators and Comparators/Iterators and Comparators - Lab/01.Library/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public IReadOnlyList<string> Authors { get; set; }
+
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors;
+        }
+    }
+}
