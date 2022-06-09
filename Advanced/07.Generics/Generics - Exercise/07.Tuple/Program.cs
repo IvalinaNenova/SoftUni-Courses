224a039c7cbce443dda2223065231cd86182ce11
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-09 11:50:04 +0300

    Completed Task 07.Tuple

---
+++ Advanced/07.Generics/Generics - Exercise/07.Tuple/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _07.Tuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Tuple<string, string> tuple1 = new Tuple<string, string>(input[0] + " " + input[1], input[2]);
+            Console.WriteLine(tuple1.ToString());
+
+            string[] input2 = Console.ReadLine().Split();
+            Tuple<string, int> tuple2 = new Tuple<string, int>(input2[0], int.Parse(input2[1]));
+            Console.WriteLine(tuple2.ToString());
+
+            string[] input3 = Console.ReadLine().Split();
+            Tuple<int, double> tuple3 = new Tuple<int, double>(int.Parse(input3[0]), double.Parse(input3[1]));
+            Console.WriteLine(tuple3.ToString());
+        }
+    }
+}
---
+++ Advanced/07.Generics/Generics - Exercise/07.Tuple/Tuple.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.Tuple
+{
+    public class Tuple<T1, T2>
+    {
+        public Tuple(T1 item1, T2 item2)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+        }
+        public object Item1 { get; set; }
+
+        public object Item2 { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Item1} -> {Item2}";
+        }
+    }
+}
