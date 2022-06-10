411d67c437361f3c0ccfa7b22622138ea91d6325
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-10 18:36:54 +0300

    Completed Task 03.Comparable Book

---
+++ Advanced/08.Iterators and Comparators/Iterators and Comparators - Lab/03.ComparableBook/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = new List<string>(authors);
+        }
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public IReadOnlyList<string> Authors { get; set; }
+
+        public int CompareTo(Book other)
+        {
+            int result = Year.CompareTo(other.Year);
+            if (result == 0)
+            {
+                result = Title.CompareTo(other.Title);
+            }
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Year}";
+        }
+    }
+}
