3e49c451aa3bfb8364bb55d359fe16f4375f7ab3
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-11 19:46:05 +0300

    Completed task 01.ListyIterator

---
+++ Advanced/08.Iterators and Comparators/Iterators and Comparators - Exercise/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+
+namespace _01.ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> list;
+        private int currentIndex = 0;
+        public ListyIterator(List<T>list)
+        {
+            this.list = list.ToList();
+        }
+
+        public bool Move()
+        {
+            if (currentIndex + 1 < list.Count)
+            {
+                currentIndex++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return currentIndex +1 < list.Count;
+        }
+
+        public void Print()
+        {
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(list[currentIndex]);
+        }
+    }
+}
---
+++ Advanced/08.Iterators and Comparators/Iterators and Comparators - Exercise/01.ListyIterator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+
+namespace _01.ListyIterator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> iterator = null;
+            string input = Console.ReadLine();
+
+            while (!input.Contains("END"))
+            {
+                string[] commands = input.Split(' ');
+                string action = commands[0];
+                switch (action)
+                {
+                    case "Create":
+                        List<string> list = commands.Skip(1).ToList();
+                        iterator = new ListyIterator<string>(list);
+                        break;
+                    case "Move":
+                        Console.WriteLine(iterator.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(iterator.HasNext());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            iterator.Print();
+                        }
+                        catch (InvalidOperationException ioe)
+                        {
+                            Console.WriteLine(ioe.Message);
+                        }
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+        public static void Create(List<string> collection)
+        {
+            var iterator = new ListyIterator<string>(collection);
+        }
+    }
+}
