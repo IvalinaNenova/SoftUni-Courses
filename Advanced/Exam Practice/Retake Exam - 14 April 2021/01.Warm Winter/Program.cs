4492898e7e1587c70a27f410d7704b6753ef9e93
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-18 22:05:32 +0300

    Completed Task 01.Warm Winter

---
+++ Advanced/Exam Practice/Retake Exam - 14 April 2021/01.Warm Winter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Warm_Winter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] input2 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var scarfs = new Queue<int>(input2);
+            var hats = new Stack<int>(input1);
+            List<int> sets = new List<int>();
+
+            while (hats.Any() && scarfs.Any())
+            {
+                int hat = hats.Pop();
+                int scarf = scarfs.Peek();
+
+                if (hat > scarf)
+                {
+                    sets.Add(hat + scarf);
+                    scarfs.Dequeue();
+                }
+                else if (hat == scarf)
+                {
+                    scarfs.Dequeue();
+                    hats.Push(hat + 1);
+                }
+            }
+
+            Console.WriteLine($"The most expensive set is: {sets.Max()}");
+            Console.WriteLine(string.Join(' ', sets));
+        }
+    }
+}
---
+++ Advanced/Exam Practice/Retake Exam - 14 April 2021/CocktailParty/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace CocktailParty
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+ 
+        }
+    }
+}
