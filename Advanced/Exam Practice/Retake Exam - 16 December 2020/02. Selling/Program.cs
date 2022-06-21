71cd3d2d3ce162fcaf60fb2c9304e655ba92a791
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-21 13:09:13 +0300

    Completed Task 02. Selling

--- Advanced/Exam Practice/Retake Exam - 16 December 2020/02. Selling/Program.cs
+++ Advanced/Exam Practice/Retake Exam - 16 December 2020/02. Selling/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _02._Selling
 {
@@ -7,25 +8,66 @@ namespace _02._Selling
 
     internal class Program
     {
-        private static int playerRow;
-        private static int playerCol;
+        private static int myRow;
+        private static int myCol;
 
         private static int newRow;
         private static int newCol;
 
         private static char[,] field;
         private static int size;
+        private static List<(int, int)> pillars;
+        private const char MySymbol = 'S';
+        private const char Pillar = 'O';
+        private const char EmptySpace = '-';
+
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int totalIncome = 0;
+            FillMatrix();
+            while (totalIncome < 50)
+            {
+                field[myRow, myCol] = EmptySpace;
+
+                string direction = Console.ReadLine();
+                (newRow, newCol) = NextPosition(myRow, myCol, direction);
+                if (HasValidCoordinates(newRow, newCol))
+                {
+                    if (field[newRow, newCol] == Pillar)
+                    {
+                        field[newRow, newCol] = EmptySpace;
+                        pillars.Remove((newRow, newCol));
+                        (newRow, newCol) = pillars[0];
+                    }
+                    else if (char.IsDigit(field[newRow, newCol]))
+                    {
+                        string digitAsString = field[newRow, newCol].ToString();
+                        totalIncome += int.Parse(digitAsString);
+                    }
+                }
+                else
+                {
+                    break;
+                }
+
+                (myRow, myCol) = (newRow, newCol);
+                field[myRow, myCol] = MySymbol;
+            }
+
+            Console.WriteLine(totalIncome >= 50
+                ? "Good news! You succeeded in collecting enough money!"
+                : "Bad news, you are out of the bakery.");
+            Console.WriteLine($"Money: {totalIncome}");
+
+            PrintOutput();
         }
 
         public static void FillMatrix()
         {
             size = int.Parse(Console.ReadLine());
             field = new char[size, size];
-
+            pillars = new List<(int, int)>();
             for (int row = 0; row < size; row++)
             {
                 string rowData = Console.ReadLine();
@@ -34,16 +76,20 @@ namespace _02._Selling
                 {
                     field[row, col] = rowData[col];
 
-                    if (field[row, col] == ' ')
+                    if (field[row, col] == MySymbol)
                     {
-                        playerRow = row;
-                        playerCol = col;
+                        myRow = row;
+                        myCol = col;
+                    }
+                    else if (field[row, col] == Pillar)
+                    {
+                        pillars.Add((row, col));
                     }
                 }
             }
         }
 
-        public static (int, int) MoveArmy(int row, int col, string direction)
+        public static (int, int) NextPosition(int row, int col, string direction)
         {
             switch (direction)
             {
