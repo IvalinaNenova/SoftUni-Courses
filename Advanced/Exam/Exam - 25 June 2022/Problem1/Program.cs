cd68b2eaaa3910fac2e4acac4e0cfca96510aa00
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-24 19:58:56 +0300

    Created solution for Advanced Exam

---
+++ Advanced/Exam/Exam - 25 June 2022/Problem1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] input2 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            var queue = new Queue<int>();
+            var stack = new Stack<int>();
+
+            while (queue.Any() && stack.Any())
+            {
+                
+            }
+
+            Console.WriteLine(queue.Any()
+                ? $"{string.Join(", ", queue)}"
+                :$"");
+            Console.WriteLine(stack.Any()
+                ?$"{string.Join(", ", stack)}"
+                :"");
+        }
+    }
+}
---
+++ Advanced/Exam/Exam - 25 June 2022/Problem2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Problem2
+{
+    internal class Program
+    {
+        private static int playerRow;
+        private static int playerCol;
+
+        private static int newRow;
+        private static int newCol;
+
+        private static char[,] field;
+        private static int size;
+
+        private const char MySymbol = ' ';
+        private const char MySymbol = ' ';
+        private const char MySymbol = ' ';
+        private const char MySymbol = ' ';
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void FillMatrix()
+        {
+            size = int.Parse(Console.ReadLine());
+            field = new char[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                string rowData = Console.ReadLine();
+
+                for (int col = 0; col < size; col++)
+                {
+                    field[row, col] = rowData[col];
+
+                    if (field[row, col] == ' ')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                    }
+                }
+            }
+        }
+
+        public static (int, int) MoveArmy(int row, int col, string direction)
+        {
+            switch (direction)
+            {
+                case "up":
+                    row--;
+                    break;
+                case "down":
+                    row++;
+                    break;
+                case "left":
+                    col--;
+                    break;
+                case "right":
+                    col++;
+                    break;
+            }
+
+            return (row, col);
+        }
+        public static void PrintOutput()
+        {
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    Console.Write(field[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static bool HasValidCoordinates(int row, int col)
+        {
+            return row >= 0 && row < field.GetLength(0) &&
+                   col >= 0 && col < field.GetLength(1);
+        }
+
+    }
+
+}
