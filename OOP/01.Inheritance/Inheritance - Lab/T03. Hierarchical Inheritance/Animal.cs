9b3c458021f31eeb036c552d929da0ca84e4f3d1
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-27 16:02:01 +0300

    Completed Task 03

---
+++ OOP/01.Inheritance/Inheritance - Lab/T03. Hierarchical Inheritance/Animal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Farm
+{
+    public  class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating…");
+        }
+    }
+}
---
+++ OOP/01.Inheritance/Inheritance - Lab/T03. Hierarchical Inheritance/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            cat.Eat();
+            cat.Meow();
+
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+        }
+    }
+}
