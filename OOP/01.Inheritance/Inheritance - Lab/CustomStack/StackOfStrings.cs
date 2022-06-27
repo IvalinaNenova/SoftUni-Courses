faa2d9ecfa7f52a36be3bef77290c9c4818429d9
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-27 16:15:26 +0300

    Completed Task 05.CustomStack

---
+++ OOP/01.Inheritance/Inheritance - Lab/CustomStack/StackOfStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return Count == 0;
+        }
+
+        public void AddRange(IEnumerable<string> items)
+        {
+            foreach (var item in items)
+            {
+                Push(item);
+            }
+        }
+    }
+}
---
+++ OOP/01.Inheritance/Inheritance - Lab/CustomStack/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<string> myList = new List<string>{"Diana", "Marina", "Vania", "Hristo", "Zari"};
+            StackOfStrings myStack = new StackOfStrings();
+            Console.WriteLine(myStack.IsEmpty());
+            myStack.AddRange(myList);
+            Console.WriteLine(myStack.Count);
+            Console.WriteLine(myStack.IsEmpty());
+        }
+    }
+}
