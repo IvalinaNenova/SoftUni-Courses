ab3b98289a144b9b3389a6dfa08e67d4c4abdcd0
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-27 16:02:25 +0300

    Completed Task 04. Random List

---
+++ OOP/01.Inheritance/Inheritance - Lab/Т04. Random List/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList {"Iva", "Marina", "Pesho", "Gosho", "Svetoslav"};
+            string random = list.RandomString();
+            Console.WriteLine(random);
+        }
+    }
+}
