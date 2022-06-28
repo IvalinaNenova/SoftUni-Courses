5fbc0dca48d1a9a389c3ffbad53d31dcd2950e9a
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-28 15:52:28 +0300

    Completed Task 04.NeedForSpeed

---
+++ OOP/01.Inheritance/Inheritance - Exercise/NeedForSpeed/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            FamilyCar familyCar = new FamilyCar(100, 50);
+            familyCar.Drive(10);
+            Console.WriteLine(familyCar.Fuel);
+
+            CrossMotorcycle cross = new CrossMotorcycle(150, 50);
+            cross.Drive(10);
+            Console.WriteLine(cross.Fuel);
+        }
+    }
+}
---
+++ OOP/01.Inheritance/Inheritance - Exercise/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double DefaultFuelConsumption = 1.25;
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+        }
+
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+
+
+        public virtual void Drive(double kilometers)
+        {
+            this.Fuel -= kilometers * FuelConsumption;
+        }
+    }
+}
