9216c526277b2834b67eee7e281311f8c41edbf5
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-30 21:12:24 +0300

    Completed Task 01.Person

---
+++ OOP/02.Encapsulation/Encapsulation - Lab/Persons/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+        }
+
+    }
+}
