60f45c809db0fbc4684081c9bbb8f9695bf5b15b
Ivalina Nenova <nenovaivalina@gmail.com>
2022-06-30 21:38:35 +0300

    Completed Task 04.Team

---
+++ OOP/02.Encapsulation/Encapsulation - Lab/Team/Person.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+
+                lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get => age;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+
+                age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get => salary;
+            private set
+            {
+                //if (value < 650)
+                //{
+                //    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                //}
+                salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age > 30)
+            {
+                percentage = 1 + percentage / 100;
+            }
+            else
+            {
+                percentage = 1 + (percentage / 100) / 2;
+            }
+            this.Salary *= percentage;
+        }
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+        }
+
+    }
+}
---
+++ OOP/02.Encapsulation/Encapsulation - Lab/Team/Team.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using PersonsInfo;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+        public IReadOnlyList<Person> FirstTeam => firstTeam;
+        public IReadOnlyList<Person> ReserveTeam => reserveTeam;
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+    }
+}
