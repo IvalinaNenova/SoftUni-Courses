ff046dfee30cb3474fe176dd38ea322e40594ef7
Ivalina Nenova <nenovaivalina@gmail.com>
2022-07-01 14:26:28 +0300

    Completed Task 01.ClassBoxData

---
+++ OOP/02.Encapsulation/Encapsulation - Exercise/ClassBoxData/Box.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+        public double Length
+        {
+            get => length;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Length cannot be zero or negative.");
+                }
+
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => width;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Width cannot be zero or negative.");
+                }
+
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => height;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Height cannot be zero or negative.");
+                }
+
+                height = value;
+            }
+        }
+
+        //Behavior
+        public double SurfaceArea() //Surface Area = 2lw + 2lh + 2wh
+        {
+            return 2 * length * width + 2 * length * height + 2 * width * height;
+        }
+
+        public double LateralSurfaceArea() //Lateral Surface Area = 2lh + 2wh
+        {
+            return 2 * length * height + 2 * width * height;
+        }
+
+        public double Volume() //Volume = lwh
+        {
+            return length * width * height;
+        }
+    }
+}
---
+++ OOP/02.Encapsulation/Encapsulation - Exercise/ClassBoxData/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            Box box;
+            try
+            {
+                box = new Box(length, width, height);
+                
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+                return;
+            }
+
+            Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+            Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+            Console.WriteLine($"Volume - {box.Volume():f2}");
+        }
+    }
+}
