ec3c6069f9e264e0e59f199f0993d537eb311ecd
Ivalina Nenova <nenovaivalina@gmail.com>
2022-07-01 14:48:10 +0300

    Completed Task 02.AnimalFarm

---
+++ OOP/02.Encapsulation/Encapsulation - Exercise/AnimalFarm/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AnimalFarm
+{
+    using AnimalFarm.Models;
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            Chicken chicken;
+            try
+            {
+                chicken = new Chicken(name, age);
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+                return;
+            }
+
+            Console.WriteLine(
+                "Chicken {0} (age {1}) can produce {2} eggs per day.",
+                chicken.Name,
+                chicken.Age,
+                chicken.ProductPerDay);
+        }
+    }
+}
