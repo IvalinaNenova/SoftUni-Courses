fef8cbfa8694a9467641757c0b1c6fcf6860f6b3
Ivalina Nenova <nenovaivalina@gmail.com>
2022-07-04 19:44:15 +0300

    Completed Task 02.Cars

---
+++ OOP/03.Interfaces And Abstraction/Interfaces And Abstraction - Lab/Cars/Seat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat :ICar
+    {
+        private string model;
+        private string color;
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+        public string Model
+        {
+            get => model;
+            set => model = value;
+        }
+
+        public string Color
+        {
+            get => color;
+            set => color = value;
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} {this.GetType().Name} {this.Model}";
+        }
+    }
+}
---
+++ OOP/03.Interfaces And Abstraction/Interfaces And Abstraction - Lab/Cars/Tesla.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar , IElectricCar
+    {
+        private string model;
+        private string color;
+        private int battery;
+
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+        public string Model
+        {
+            get => model;
+            set => model = value;
+        }
+
+        public string Color
+        {
+            get => color;
+            set => color = value;
+        }
+
+        public int Battery
+        {
+            get => battery;
+            set => battery = value;
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} {this.GetType().Name} {this.Model} with {this.Battery} Batteries";
+        }
+    }
+}
