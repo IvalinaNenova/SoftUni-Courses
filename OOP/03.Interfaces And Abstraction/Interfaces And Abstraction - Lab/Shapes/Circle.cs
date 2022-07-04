8748f1493baf505808f49ba7b05500065ca67e80
Ivalina Nenova <nenovaivalina@gmail.com>
2022-07-04 19:23:25 +0300

    Completed Task 01.Shapes

---
+++ OOP/03.Interfaces And Abstraction/Interfaces And Abstraction - Lab/Shapes/Circle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        private int radius;
+
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
+
+        public int Radius
+        {
+            get => radius;
+            set => radius = value;
+        }
+
+        public void Draw()
+        {
+            double rIn = radius - 0.4;
+            double rOut = radius + 0.4;
+
+            for (double i = radius; i >= -radius; i--)
+            {
+                for (double j = -radius; j < rOut; j += 0.5)
+                {
+                    double value = i* i + j* j;
+
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                    {
+                        Console.Write('*');
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ OOP/03.Interfaces And Abstraction/Interfaces And Abstraction - Lab/Shapes/Rectangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Width
+        {
+            get => width;
+            set => width = value;
+        }
+
+        public int Height
+        {
+            get => height;
+            set => height = value;
+        }
+
+
+        public void Draw()
+        {
+            Console.WriteLine(new string ('*', width));
+
+            for (int i = 1; i < height - 1; i++)
+            {
+                Console.WriteLine('*' + new string(' ', width -2) + '*');
+            }
+
+            Console.WriteLine(new string('*', width));
+        }
+    }
+}
