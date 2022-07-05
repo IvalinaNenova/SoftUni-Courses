c9f3fe7498e7a6203a24be7f835e7a128ea0f29f
Ivalina Nenova <nenovaivalina@gmail.com>
2022-07-05 13:42:50 +0300

    Completed Task 01.Define Interface IPerson

---
+++ OOP/03.Interfaces And Abstraction/Interfaces And Abstraction - Exercise/T01.DefineIPerson/Citizen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+    }
+}
