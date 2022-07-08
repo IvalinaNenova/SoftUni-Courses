8c3cb571024e01cc74ae509190ffb8a117d8386a
Ivalina Nenova <nenovaivalina@gmail.com>
2022-07-08 19:12:38 +0300

    Completed Task 01.MathOperations

---
+++ OOP/04.Polymorphism/Polymorphism - Lab/MathOperations/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Operations
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            MathOperations operations = new MathOperations();
+
+            Console.WriteLine(operations.Add(2, 3));
+            Console.WriteLine(operations.Add(2.2, 3.3, 5.5));
+            Console.WriteLine(operations.Add(2.2m, 3.3m, 4.4m));
+        }
+    }
+}
