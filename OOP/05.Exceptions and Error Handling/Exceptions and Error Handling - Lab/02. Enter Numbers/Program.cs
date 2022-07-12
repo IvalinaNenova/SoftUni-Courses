502582454a88a8beccc0e5c1eeae327d266c98ab
Ivalina Nenova <nenovaivalina@gmail.com>
2022-07-12 16:33:54 +0300

    Completed Task 02. Enter Numbers

---
+++ OOP/05.Exceptions and Error Handling/Exceptions and Error Handling - Lab/02. Enter Numbers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._Enter_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            int start = 1;
+            while (numbers.Count != 10)
+            {
+                int end = 100;
+                try
+                {
+                    start = ReadNumber(start, end);
+                    numbers.Add(start);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+
+        public static int ReadNumber(int start, int end)
+        {
+            string number = Console.ReadLine();
+           
+            if (!int.TryParse(number, out int result))
+            {
+                throw new FormatException("Invalid Number!");
+            }
+
+            if (result <= start || result >= end)
+            {
+                throw new ArgumentException($"Your number is not in range {start} - 100!");
+            }
+
+            return result;
+        }
+    }
+}
