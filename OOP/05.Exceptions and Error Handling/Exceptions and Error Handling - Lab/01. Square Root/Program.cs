35dd5a5976366b5b6b230111575bddc5a0347f19
Ivalina Nenova <nenovaivalina@gmail.com>
2022-07-12 16:16:42 +0300

    Completed Task 01. Square Root

---
+++ OOP/05.Exceptions and Error Handling/Exceptions and Error Handling - Lab/01. Square Root/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01._Square_Root
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                double result = GetSqrt(number);
+                Console.WriteLine(result);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+
+        public static double GetSqrt(double number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException("Invalid number.");
+            }
+
+            return Math.Sqrt(number);
+        }
+    }
+}
