31ce9b0bcb4861f7abdee159e52c9507ba5df3cd
Ivalina Nenova <nenovaivalina@gmail.com>
2022-07-16 15:18:22 +0300

    Completed Task 01.Stealer

---
+++ OOP/06.Reflection And Attributes/Reflection And Attributes - Lab/Stealer/Spy.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace Stealer
+{
+    using System;
+    using System.Text;
+
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] requestedFields)
+        {
+            Type classType = Type.GetType(className);
+            FieldInfo[] classFields = classType.GetFields(BindingFlags.Public |
+                                                                    BindingFlags.NonPublic |
+                                                                    BindingFlags.Static |
+                                                                    BindingFlags.Instance);
+
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Class under investigation: {classType.FullName}");
+
+            foreach (var field in classFields.Where(field => requestedFields.Contains(field.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+
+    }
+}
---
+++ OOP/06.Reflection And Attributes/Reflection And Attributes - Lab/Stealer/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+
+            string missionResult = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+
+            Console.WriteLine(missionResult);
+        }
+    }
+
+
+}
