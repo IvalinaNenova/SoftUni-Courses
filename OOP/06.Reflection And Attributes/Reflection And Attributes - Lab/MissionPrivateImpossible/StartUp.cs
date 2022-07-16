47c62af4e34d3f63e449b8dc91abcd48df59f758
Ivalina Nenova <nenovaivalina@gmail.com>
2022-07-16 15:49:00 +0300

    Completed Task 03. Mission Private Impossible

---
+++ OOP/06.Reflection And Attributes/Reflection And Attributes - Lab/MissionPrivateImpossible/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace Stealer
+{
+    using System;
+
+    public class Startup
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+
+            string result = spy.RevealPrivateMethods("Stealer.Hacker");
+
+            Console.WriteLine(result);
+        }
+    }
+}
