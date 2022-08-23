6461eeac9c8c5ffb897e209c1b78a0b63dbe11f6
Ivalina Nenova <nenovaivalina@gmail.com>
2022-08-23 16:28:02 +0300

    Completed task 04.Circle Area

---
+++ JavaScript Advanced/01.Syntax, Functions and Statements/Lab/04-circle-area.js
@@ -0,0 +1,13 @@
+function solve(param){
+    let type = typeof(param);
+    let result;
+    if (type == 'number') {
+        result = (Math.PI*param**2).toFixed(2);
+    } else {
+        result = (`We can not calculate the circle area, because we receive a ${type}.`);
+    }
+    console.log(result);
+}
+
+solve(5);
+solve('name');
