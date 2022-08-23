cfe5ebeac64eb216dd75713ec210ef90fef7673c
Ivalina Nenova <nenovaivalina@gmail.com>
2022-08-23 19:25:13 +0300

    Completed Task 10. Agregate Elements

---
+++ JavaScript Advanced/01.Syntax, Functions and Statements/Lab/10.aggregate-elements.js
@@ -0,0 +1,19 @@
+function solve(array) {
+    let numbersArray = array.map(Number);
+    let sum = numbersArray.reduce((a, b) => a + b);
+
+    let inverseValuesSum = 0;
+
+    for (let i = 0; i < numbersArray.length; i++) {
+        inverseValuesSum += 1 / numbersArray[i];
+    }
+
+    let arrayAsString = array.join('');
+
+    console.log(sum);
+    console.log(inverseValuesSum);
+    console.log(arrayAsString);
+}
+
+solve([1, 2, 3]);
+solve([2, 4, 8, 16]);
