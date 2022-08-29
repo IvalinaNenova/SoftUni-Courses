d255930de919d4baffedfc16a9afed8bd458fba3
Ivalina Nenova <nenovaivalina@gmail.com>
2022-08-29 13:11:45 +0300

    Completed Task 07.Piece of pie

---
+++ JavaScript Advanced/02.Arrays and Nested Arrays/Lab/07.piece-of-pie.js
@@ -0,0 +1,23 @@
+function solve(array, start, end) {
+    
+    return array.slice(array.indexOf(start), array.indexOf(end) + 1);
+}
+
+console.log(solve(['Pumpkin Pie',
+'Key Lime Pie',
+'Cherry Pie',
+'Lemon Meringue Pie',
+'Sugar Cream Pie'],
+'Key Lime Pie',
+'Lemon Meringue Pie'
+));
+
+console.log(solve(['Apple Crisp',
+'Mississippi Mud Pie',
+'Pot Pie',
+'Steak and Cheese Pie',
+'Butter Chicken Pie',
+'Smoked Fish Pie'],
+'Pot Pie',
+'Smoked Fish Pie'
+));
