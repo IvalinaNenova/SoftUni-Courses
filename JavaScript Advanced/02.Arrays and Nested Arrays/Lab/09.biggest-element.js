847d731621754c414d16965e05656adb902fbbc4
Ivalina Nenova <nenovaivalina@gmail.com>
2022-08-29 17:50:51 +0300

    Completed Task 09.Biggest element

---
+++ JavaScript Advanced/02.Arrays and Nested Arrays/Lab/09.biggest-element.js
@@ -0,0 +1,23 @@
+function solve(nestedArray) {
+
+    let biggestNum = -10000000;
+    for (let row = 0; row < nestedArray.length; row++) {
+
+        let currentBiggest = Math.max(...nestedArray[row]);
+
+        if (currentBiggest >= biggestNum) {
+            biggestNum = currentBiggest;
+        }
+    }
+
+    return biggestNum;
+}
+
+console.log(solve([[20, 50, 10],
+                   [8, 33, 145]]
+   ));  
+
+console.log(solve([[3, 5, 7, 12],
+                   [-1, 4, 33, 2],
+                   [8, 3, 0, 4]]
+   ));
