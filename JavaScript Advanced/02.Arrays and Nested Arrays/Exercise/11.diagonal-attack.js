77c630932974798946a447db32db8d4a7f6064a6
Ivalina Nenova <nenovaivalina@gmail.com>
2022-08-30 19:00:21 +0300

    Removed fillMatrix function

--- JavaScript Advanced/02.Arrays and Nested Arrays/Exercise/11.diagonal-attack.js
+++ JavaScript Advanced/02.Arrays and Nested Arrays/Exercise/11.diagonal-attack.js
@@ -1,15 +1,17 @@
 function solve(array) {
 
-    let squareMatrix = fillMatrix(array);
-    let size = squareMatrix.length;
+    let numbersMatrix = [];
+    array.forEach(el => numbersMatrix.push(el.split(' ').map(Number)));
+    
+    let size = numbersMatrix.length;
 
     let rightDiagonalSum = 0;
     let leftDiagonalSum = 0;
 
     for (let row = 0; row < size; row++) {
 
-        rightDiagonalSum += squareMatrix[row][row];
-        leftDiagonalSum += squareMatrix[row][size - 1 - row];
+        rightDiagonalSum += numbersMatrix[row][row];
+        leftDiagonalSum += numbersMatrix[row][size - 1 - row];
     }
 
     if (rightDiagonalSum == leftDiagonalSum) {
@@ -17,23 +19,13 @@ function solve(array) {
         for (let row = 0; row < size; row++) {
             for (let col = 0; col < size; col++) {
                 if (col != size - 1 - row && row != col) {
-                    squareMatrix[row][col] = rightDiagonalSum;
+                    numbersMatrix[row][col] = rightDiagonalSum;
                 }
             }           
         }
     } 
 
-    squareMatrix.forEach(el => console.log(el.join(' ')));
-
-    function fillMatrix(array) {
-        let matrix = [];
-
-        for (let row = 0; row < array.length; row++) {
-            matrix[row] = array[row].split(' ').map(x => Number(x));
-        }
-
-        return matrix;
-    }
+    numbersMatrix.forEach(el => console.log(el.join(' ')));
 }
 
 solve(['5 3 12 3 1',
