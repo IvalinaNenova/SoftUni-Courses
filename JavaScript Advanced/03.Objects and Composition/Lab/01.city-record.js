15cd00f67a1092c1ee33767bc23064d541c60fa4
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-03 10:40:08 +0300

    Completed task 01.City records

---
+++ JavaScript Advanced/03.Objects and Composition/Lab/01.city-record.js
@@ -0,0 +1,11 @@
+function solve(city, population, treasury) {
+    let info = {
+        name: city,
+        population,
+        treasury,
+    }
+
+    return info;
+}
+
+console.log(solve('Tortuga', 7000, 15000));
