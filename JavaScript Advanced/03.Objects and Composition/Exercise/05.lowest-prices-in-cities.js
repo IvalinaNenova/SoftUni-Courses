51bc59d023be55ee3af73976ef5c91eee8fc7eab
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-04 15:53:06 +0300

    Completed Task 05.Lowest price in cities

---
+++ JavaScript Advanced/03.Objects and Composition/Exercise/05.lowest-prices-in-cities.js
@@ -0,0 +1,35 @@
+function solve(arr){
+    let result = [];
+
+    for (const line of arr) {
+        let [town, product, price] = line.split(' | ');
+        price = Number(price);
+      
+        let object = result.find(x=> x.product === product);
+        if (object) {
+            
+            if (price < object.price) {
+                object.price = price;
+                object.town = town;
+            }
+        }
+        else{
+        object = {product,price,town};
+        result.push(object);
+        }
+       
+    }
+
+    for (let item of result) {
+        console.log(`${item.product} -> ${item.price} (${item.town})`);
+    }
+       
+}
+
+solve(['Sample Town | Sample Product | 1000',
+'Sample Town | Orange | 2',
+'Sample Town | Peach | 1',
+'Sofia | Orange | 3',
+'Sofia | Peach | 2',
+'New York | Sample Product | 1000.1',
+'New York | Burger | 10']);
