2291177e8505154530e6ef29c8d6236e14d4ef0f
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-15 17:35:58 +0300

    Completed Task 02.Collect List Items

--- JavaScript Advanced/04.DOM Introduction/Lab/02.Collect List Items/CollectListItems.js
+++ JavaScript Advanced/04.DOM Introduction/Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,6 @@
 function extractText() {
-    // TODO
+    let listElements = document.getElementById('items');
+
+    let textArea = document.getElementById('result');
+    textArea.value = listElements.textContent;
 }
