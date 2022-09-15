838c7c6360dc837654cf447380307d18f5980782
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-15 19:37:05 +0300

    Completed Task 07.Extract Parentesis

--- JavaScript Advanced/04.DOM Introduction/Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ JavaScript Advanced/04.DOM Introduction/Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,14 @@
 function extract(content) {
+    let contentElement = document.getElementById(content);
+        
+   
+    let pattern = /\(([^(]+)\)/g;
+    let matches = contentElement.textContent.matchAll(pattern);
 
+    let matchesArray = [];
+    for (const match of matches) {
+        matchesArray.push(match[1]);
+    }
+    
+    return matchesArray.join('; ');
 }
