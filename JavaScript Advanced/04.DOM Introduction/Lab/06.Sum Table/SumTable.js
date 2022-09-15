41b96ef82d17aa1be85b7685a7fbc850e759e433
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-15 19:03:10 +0300

    Completed Task 06.Sum Table

--- JavaScript Advanced/04.DOM Introduction/Lab/06.Sum Table/SumTable.js
+++ JavaScript Advanced/04.DOM Introduction/Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,13 @@
 function sumTable() {
+    let costElements = document.querySelectorAll('tr td:nth-of-type(2)');
 
+    let costCells = Array.from(costElements);
+    costCells.pop();
+    
+    let sum = costCells.reduce((a, x) => {
+       return a + Number(x.textContent);
+    }, 0)
+
+    let resultElement = document.getElementById('sum');
+    resultElement.textContent = sum;
 }
