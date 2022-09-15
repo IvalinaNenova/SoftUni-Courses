089e9f2362e6a28407c2d409e81f5b756fa5f54c
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-15 18:47:18 +0300

    Completed Task 05.Colorize Table

--- JavaScript Advanced/04.DOM Introduction/Lab/05.Colorize Table/ColorizeTable.js
+++ JavaScript Advanced/04.DOM Introduction/Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,17 @@
 function colorize() {
-    // TODO
+    let tableElements = document.querySelectorAll('tr:nth-of-type(2n)');
+    let tableElementsArray = Array.from(tableElements);
+    
+    tableElementsArray.forEach(x => {
+        x.style.backgroundColor = 'teal';
+    })
+
+    // let tableElements = document.getElementsByTagName('tr');
+    // let rows = Array.from(tableElements);
+
+    // rows.forEach((x, i) =>{
+    //     if (i % 2 != 0 ) {
+    //         x.style.backgroundColor = 'teal';
+    //     }
+    // })
 }
