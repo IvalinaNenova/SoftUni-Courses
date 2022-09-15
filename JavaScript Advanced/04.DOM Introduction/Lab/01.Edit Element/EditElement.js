5d571b02b15aebf63a5a14389fa5a835d51ce4f2
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-15 17:27:37 +0300

    Completed Task 01.Edit Element

--- JavaScript Advanced/04.DOM Introduction/Lab/01.Edit Element/EditElement.js
+++ JavaScript Advanced/04.DOM Introduction/Lab/01.Edit Element/EditElement.js
@@ -1,3 +1,10 @@
-function editElement() {
-    // TODO
+function editElement(element, match, replacer) {
+
+    // while(element.textContent.includes(match)){
+    //     element.textContent = element.textContent.replace(match, replacer);
+    // }
+
+    let pattern = new RegExp(match, 'g');
+    element.textContent = element.textContent.replace(pattern, replacer);
+
 }
