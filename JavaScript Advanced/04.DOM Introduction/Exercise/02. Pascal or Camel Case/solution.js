2e094a2ee7de4583067f1146bb6c856a6aad68d6
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-17 18:05:05 +0300

    Completed Task 03.Accordeon

--- JavaScript Advanced/04.DOM Introduction/Exercise/02. Pascal or Camel Case/solution.js
+++ JavaScript Advanced/04.DOM Introduction/Exercise/02. Pascal or Camel Case/solution.js
@@ -3,32 +3,30 @@ function solve() {
 
   let toCamelCase = (input) => {
     result = input
-    .toLowerCase()
-    .split(' ')
-    .map((word, index) => index === 0 ? word : word[0].toUpperCase() + word.substring(1, word.length))
-    .join('');
+      .toLowerCase()
+      .split(' ')
+      .map((word, index) => index === 0 ? word : word[0].toUpperCase() + word.substring(1, word.length))
+      .join('');
   }
 
   let toPascalCase = (input) => {
     result = input
-    .toLowerCase()
-    .split(' ')
-    .map((word) =>  word[0].toUpperCase() + word.substring(1, word.length))
-    .join('');
+      .toLowerCase()
+      .split(' ')
+      .map((word) => word[0].toUpperCase() + word.substring(1, word.length))
+      .join('');
     return result;
   }
 
   let input = document.getElementById('text').value;
   let namingConvention = document.getElementById('naming-convention').value;
 
-
   switch (namingConvention) {
     case 'Camel Case': toCamelCase(input);
       break;
-      case 'Pascal Case': toPascalCase(input);
+    case 'Pascal Case': toPascalCase(input);
       break;
-      default: result = 'Error!';
-
+    default: result = 'Error!';
   }
 
   let resultElement = document.getElementById('result');
--- JavaScript Advanced/04.DOM Introduction/Exercise/03. Accordion/accordion.js
+++ JavaScript Advanced/04.DOM Introduction/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    let buttonElement = document.getElementsByClassName('button')[0];
+    let divElement = document.getElementById('extra');
+
+    if (buttonElement.textContent == 'More') {
+        buttonElement.textContent = 'Less';
+        divElement.style.display = 'block';
+    } else {
+        buttonElement.textContent = 'More';
+        divElement.style.display = 'none';
+    }
 }
