15213210a7580867bf4c0aa68620cd9273ab9318
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-17 19:04:39 +0300

    Completed Task 04.Search in List

--- JavaScript Advanced/04.DOM Introduction/Exercise/04. Search in List/search.js
+++ JavaScript Advanced/04.DOM Introduction/Exercise/04. Search in List/search.js
@@ -1,3 +1,20 @@
 function search() {
-   // TODO
+   let textToSearch = document.getElementById('searchText').value;
+   let townsElements = Array.from(document.querySelectorAll('#towns li'));
+   let resultElement = document.getElementById('result');
+
+   let count = 0;
+
+   for (const town of townsElements) {
+      if (town.textContent.includes(textToSearch)) {
+         count++;
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration = 'underline';
+      }else{
+         town.style.fontWeight = 'normal';
+         town.style.textDecoration = 'none';
+      }
+   }
+
+   resultElement.textContent = `${count} matches found`;
 }
