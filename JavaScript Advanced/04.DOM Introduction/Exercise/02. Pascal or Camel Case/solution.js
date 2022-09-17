a23c964cd798ed30a9ff76f0504057cc737f9ae1
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-17 17:41:20 +0300

    Completed Task 02.Pascal or Camel Case

--- JavaScript Advanced/04.DOM Introduction/Exercise/02. Pascal or Camel Case/solution.js
+++ JavaScript Advanced/04.DOM Introduction/Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,36 @@
 function solve() {
-  //TODO...
+  let result = '';
+
+  let toCamelCase = (input) => {
+    result = input
+    .toLowerCase()
+    .split(' ')
+    .map((word, index) => index === 0 ? word : word[0].toUpperCase() + word.substring(1, word.length))
+    .join('');
+  }
+
+  let toPascalCase = (input) => {
+    result = input
+    .toLowerCase()
+    .split(' ')
+    .map((word) =>  word[0].toUpperCase() + word.substring(1, word.length))
+    .join('');
+    return result;
+  }
+
+  let input = document.getElementById('text').value;
+  let namingConvention = document.getElementById('naming-convention').value;
+
+
+  switch (namingConvention) {
+    case 'Camel Case': toCamelCase(input);
+      break;
+      case 'Pascal Case': toPascalCase(input);
+      break;
+      default: result = 'Error!';
+
+  }
+
+  let resultElement = document.getElementById('result');
+  resultElement.textContent = result;
 }
