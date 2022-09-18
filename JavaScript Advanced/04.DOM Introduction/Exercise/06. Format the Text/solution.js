5ec6186e77493f0f99f1507982b5b0fe06e9c2ef
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-18 14:03:30 +0300

    Completed Task 06.Format the text

--- JavaScript Advanced/04.DOM Introduction/Exercise/06. Format the Text/solution.js
+++ JavaScript Advanced/04.DOM Introduction/Exercise/06. Format the Text/solution.js
@@ -1,3 +1,12 @@
 function solve() {
-  //TODO
+  let textElement = document.getElementById('input').value;
+  let sentences = Array.from(textElement.split('.').filter(sentence => sentence !== '\n' && sentence !== ''));
+  let resultDivElement = document.getElementById('output');
+
+  while (sentences.length > 0) {
+    let text = sentences.splice(0, 3).join('.') + '.';
+    let paragraph = document.createElement('p');
+    paragraph.textContent = text;
+    resultDivElement.appendChild(paragraph);
+  }
 }
