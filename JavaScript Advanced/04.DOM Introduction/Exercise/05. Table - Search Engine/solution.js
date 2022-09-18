1d6ead827cebca4cae35da9b9ba871c511412c59
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-18 11:40:51 +0300

    Completed Task 05.Table - Search Engine

--- JavaScript Advanced/04.DOM Introduction/Exercise/05. Table - Search Engine/solution.js
+++ JavaScript Advanced/04.DOM Introduction/Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,17 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+   let searchElement = document.getElementById('searchField');
+   let rowsElement = document.querySelectorAll('tbody tr');
 
+   function onClick() {
+      for (let row of rowsElement) {
+         row.classList.remove('select');
+         if (row.textContent.includes(searchElement.value)) {
+            row.className = 'select';
+         }
+      };
+
+      searchElement.value = '';
    }
 }
