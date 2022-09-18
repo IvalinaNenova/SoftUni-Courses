aa3d20eaacb2598daa8322932fd8c2d2cae03df8
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-18 17:04:10 +0300

    Completed Task 07.Hell's Kitchen - 90/100

--- JavaScript Advanced/04.DOM Introduction/Exercise/07. Hell's Kitchen/app.js
+++ JavaScript Advanced/04.DOM Introduction/Exercise/07. Hell's Kitchen/app.js
@@ -1,8 +1,53 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
-   function onClick () {
-      //   TODO:
-      
+   function onClick() {
+
+      let input = JSON.parse(document.querySelector('#inputs textarea').value);
+      let allRestaurants = {};
+      let bestAverageSalary = 0;
+      let bestRestaurantName = '';
+
+      for (let i = 0; i < input.length; i++) {
+
+         let [restaurantName, workers] = input[i].split(' - ');
+         let individualWorkers = workers.split(', ');
+
+         for (let worker of individualWorkers) {
+            let [workerName, salary] = worker.split(' ');
+
+            if (!allRestaurants.hasOwnProperty(restaurantName)) {
+               allRestaurants[restaurantName] = {};
+            }
+            allRestaurants[restaurantName][workerName] = Number(salary);
+         }
+
+         let entries = Object.entries(allRestaurants);
+
+         for (let [name, workers] of entries) {
+            let salaries = Object.values(workers);
+            let totalSalariesPaid = 0;
+
+            for (const salary of salaries) {
+               totalSalariesPaid += salary;
+            }
+
+            let averageSalary = totalSalariesPaid / salaries.length;
+
+            if (averageSalary > bestAverageSalary) {
+               bestAverageSalary = averageSalary;
+               bestRestaurantName = name;
+            }
+         }
+      }
+      let workersOrdered = Object.entries(allRestaurants[bestRestaurantName]).sort((a, b) => b[1] - a[1]);
+      let workersFormatedArray = [];
+      workersOrdered.reduce((result, worker) => {
+         result.push(`Name: ${worker[0]} With Salary: ${worker[1]}`);
+         return result;
+      }, workersFormatedArray);
+
+      document.querySelector('#bestRestaurant p').textContent = `Name: ${bestRestaurantName} Average Salary: ${bestAverageSalary.toFixed(2)} Best Salary: ${workersOrdered[0][1].toFixed(2)}`;
+      document.querySelector('#workers p').textContent = workersFormatedArray.join(' ');
    }
 }
