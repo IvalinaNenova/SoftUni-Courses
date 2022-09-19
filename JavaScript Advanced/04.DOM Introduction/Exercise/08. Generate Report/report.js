12a53c9d77e30b61299d56aba427d7e6e6c21585
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-19 12:53:02 +0300

    Completed Task 08.Generate report

--- JavaScript Advanced/04.DOM Introduction/Exercise/08. Generate Report/report.js
+++ JavaScript Advanced/04.DOM Introduction/Exercise/08. Generate Report/report.js
@@ -1,3 +1,37 @@
 function generateReport() {
-    //TODO
+    let checkedBoxesIndices = [];
+    let checkedBoxes = document.querySelectorAll('input');
+
+    // checkedBoxes.forEach((el, index) => {
+    //     if (el.checked) {
+    //         checkedBoxesIndices.push(index);
+    // };
+    
+    for (let i = 0; i < checkedBoxes.length; i++) {
+        const element = checkedBoxes[i];
+
+        if (element.checked) {
+            checkedBoxesIndices.push(i);
+        }
+    }
+
+    let result = [];
+    let rowsElements = document.querySelectorAll('tbody tr');
+    let rows = Array.from(rowsElements);
+
+    for (const row of rows) {
+        let object = {};
+
+        for (const index of checkedBoxesIndices) {
+            let propertyName = checkedBoxes[index].name;
+            let propertyData = row.children[index].textContent;
+            object[propertyName] = propertyData;
+        }
+
+        result.push(object);
+    }
+
+    let report = JSON.stringify(result);
+    let outputElement = document.getElementById('output');
+    outputElement.textContent = report;
 }
