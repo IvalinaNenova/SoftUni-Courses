58f6a61fe341f79dc4aca50dfbed07019341fe65
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-19 15:07:58 +0300

    Completed task 09.Number Converter

--- JavaScript Advanced/04.DOM Introduction/Exercise/09. Number Convertor/solution.js
+++ JavaScript Advanced/04.DOM Introduction/Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,28 @@
 function solve() {
+    let selectToElement = document.getElementById('selectMenuTo');
 
-    //TODO...
+    let optionHexadecimal = document.createElement('option');
+    optionHexadecimal.value = 'hexadecimal';
+    optionHexadecimal.textContent = 'Hexadecimal';
+
+    let optionBinary = document.createElement('option');
+    optionBinary.value = 'binary';
+    optionBinary.textContent = 'Binary';
+    
+    selectToElement.appendChild(optionHexadecimal);
+    selectToElement.appendChild(optionBinary);
+
+    let buttonElement = document.getElementsByTagName('button')[0];
+
+    buttonElement.addEventListener('click', () => {
+
+        let result = document.getElementById('result');
+        let inputElement = document.getElementById('input');
+        let inputNumber = Number(inputElement.value);
+        if (selectToElement.value == 'binary') {
+            result.value = inputNumber.toString(2);
+        }else if (selectToElement.value = 'hexadecimal'){
+            result.value = inputNumber.toString(16).toUpperCase();
+        }
+    });
 }
