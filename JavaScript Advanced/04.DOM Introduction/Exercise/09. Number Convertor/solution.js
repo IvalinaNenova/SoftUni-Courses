393c1409a55f6b2e6fbb6e7078642c88a610f50c
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-19 15:09:10 +0300

    refactored code

--- JavaScript Advanced/04.DOM Introduction/Exercise/09. Number Convertor/solution.js
+++ JavaScript Advanced/04.DOM Introduction/Exercise/09. Number Convertor/solution.js
@@ -8,7 +8,7 @@ function solve() {
     let optionBinary = document.createElement('option');
     optionBinary.value = 'binary';
     optionBinary.textContent = 'Binary';
-    
+
     selectToElement.appendChild(optionHexadecimal);
     selectToElement.appendChild(optionBinary);
 
@@ -17,11 +17,13 @@ function solve() {
     buttonElement.addEventListener('click', () => {
 
         let result = document.getElementById('result');
+
         let inputElement = document.getElementById('input');
         let inputNumber = Number(inputElement.value);
+
         if (selectToElement.value == 'binary') {
             result.value = inputNumber.toString(2);
-        }else if (selectToElement.value = 'hexadecimal'){
+        } else if (selectToElement.value = 'hexadecimal') {
             result.value = inputNumber.toString(16).toUpperCase();
         }
     });
