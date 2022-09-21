d1290d51e01a4fa0b20a526d2eee603c605938f4
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-21 17:55:35 +0300

    Completed Task 05.Highlight Active

--- JavaScript Advanced/05.DOM Manipulation and Events/Lab/05. Highlight-Active/app.js
+++ JavaScript Advanced/05.DOM Manipulation and Events/Lab/05. Highlight-Active/app.js
@@ -1,3 +1,16 @@
 function focused() {
-    console.log('TODO:...');
+    let inputElements = document.querySelectorAll('input');
+
+    let focus = function (e) {
+        e.target.parentNode.classList.add('focused');
+    }
+
+    let unFocus = function (e) {
+        e.target.parentNode.classList.remove('focused');
+    };
+
+    for (const el of inputElements) {
+        el.addEventListener('focus', focus);
+        el.addEventListener('blur', unFocus);
+    }
 }
