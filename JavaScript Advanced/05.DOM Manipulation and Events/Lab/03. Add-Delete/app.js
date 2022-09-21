e5fe88096694572a47fb6c20bcc1256f3bb8ef63
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-21 15:11:05 +0300

    Completed Task 03.Add/Delete

--- JavaScript Advanced/05.DOM Manipulation and Events/Lab/03. Add-Delete/app.js
+++ JavaScript Advanced/05.DOM Manipulation and Events/Lab/03. Add-Delete/app.js
@@ -1,3 +1,21 @@
 function addItem() {
-    //TODO...
+    let listElement = document.getElementById('items');
+    let inputElement = document.getElementById('newItemText');
+
+    let listItem = document.createElement('li');
+    listItem.textContent = inputElement.value;
+
+    let linkElement = document.createElement('a');
+    linkElement.href = '#';
+    linkElement.textContent = '[Delete]';
+
+    listItem.appendChild(linkElement);
+    listElement.appendChild(listItem);
+
+    linkElement.addEventListener('click', () => {
+        linkElement.parentNode.remove();
+    });
+
+    inputElement.value = '';
+    inputElement.focus();
 }
