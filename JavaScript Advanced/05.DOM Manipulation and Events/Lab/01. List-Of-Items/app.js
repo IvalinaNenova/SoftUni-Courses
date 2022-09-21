2770c9badf469b25c4adfd0c33989b8ddc583b75
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-21 13:53:43 +0300

    Completed Task 01.List of Items

--- JavaScript Advanced/05.DOM Manipulation and Events/Lab/01. List-Of-Items/app.js
+++ JavaScript Advanced/05.DOM Manipulation and Events/Lab/01. List-Of-Items/app.js
@@ -1,3 +1,9 @@
 function addItem() {
-    console.log('TODO:...');
+    let itemElements = document.getElementById('items');
+    let inputElement = document.getElementById('newItemText');
+
+    let newItemElement = document.createElement('li');
+    newItemElement.textContent = inputElement.value;
+    itemElements.appendChild(newItemElement);
+    inputElement.value = '';
 }
