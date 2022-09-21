2df64e1c35b3a4f246b5dda633466ffa32b0f2b9
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-21 15:55:16 +0300

    Completed Task 04.Mouse Gradient

--- JavaScript Advanced/05.DOM Manipulation and Events/Lab/04. Mouse-Gradient/app.js
+++ JavaScript Advanced/05.DOM Manipulation and Events/Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,12 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradientElement = document.getElementById('gradient');
+    let resultElement = document.getElementById('result');
+
+    gradientElement.addEventListener('mousemove', (e) => {
+        let maxWidth = e.target.clientWidth;
+        let pointerPosition = e.offsetX;
+        let percentage = Math.floor((pointerPosition / maxWidth) * 100);
+
+        resultElement.textContent = `${percentage}%`;
+    });
 }
