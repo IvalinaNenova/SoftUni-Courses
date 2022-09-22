16099015cff43e7af6564215b4240fb644355649
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-22 13:33:03 +0300

    Completed Task 06.Dynamic validation

--- JavaScript Advanced/05.DOM Manipulation and Events/Lab/06. Dynamic-Validation/app.js
+++ JavaScript Advanced/05.DOM Manipulation and Events/Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    console.log('TODO:...');
+    let inputElement = document.getElementById('email');
+    let pattern = /[a-z]+@[a-z]+.[a-z]+/g;
+
+    let validate = function (e) {
+        if (pattern.test(e.currentTarget.value)) {
+            e.currentTarget.removeAttribute('class');
+        } else {
+            e.currentTarget.classList.add('error');
+        }
+    }
+
+    inputElement.addEventListener('change', validate);
 }
