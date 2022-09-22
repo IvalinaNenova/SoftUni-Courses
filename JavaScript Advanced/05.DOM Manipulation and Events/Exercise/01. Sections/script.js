5be91b1dd46010673f0c3c215267c044b6358781
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-22 16:17:17 +0300

    Completed Task 01.Sections

--- JavaScript Advanced/05.DOM Manipulation and Events/Exercise/01. Sections/script.js
+++ JavaScript Advanced/05.DOM Manipulation and Events/Exercise/01. Sections/script.js
@@ -1,3 +1,20 @@
 function create(words) {
-   console.log('TODO:...');
+   let divsArray = [];
+   let contentElement = document.getElementById('content');
+
+   let displayParagraph = function (e) {
+      e.target.firstChild.style.display = 'block';
+   }
+   words.forEach(el => {
+      divElement = document.createElement('div');
+      paragraphElement = document.createElement('p');
+
+      paragraphElement.textContent = el;
+      paragraphElement.style.display = 'none';
+
+      divElement.appendChild(paragraphElement);
+      contentElement.appendChild(divElement);
+      
+      divElement.addEventListener('click', displayParagraph);
+   });
 }
