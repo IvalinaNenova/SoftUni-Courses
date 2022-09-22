c0ac49f2ee26c8f20e3a567cf7e2a7627385a662
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-22 15:14:36 +0300

    Completed Task 07.ShoppingvList

--- JavaScript Advanced/05.DOM Manipulation and Events/Lab/07. Shopping-Cart/solution.js
+++ JavaScript Advanced/05.DOM Manipulation and Events/Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,36 @@
 function solve() {
-   //TODO...
+   let addButtonsElement = document.querySelectorAll('button[class="add-product"]');
+   let textAreaElement = document.getElementsByTagName('textarea')[0];
+   let checkOutButtonElement = document.getElementsByClassName('checkout')[0];
+
+   let totalPriceToPay = 0;
+   let productsBought = [];
+
+   let addProductToCart = function (e) {
+      let productName = e.currentTarget.parentNode.parentNode.getElementsByClassName('product-title')[0].textContent;
+      let productPrice = e.currentTarget.parentNode.parentNode.getElementsByClassName('product-line-price')[0].textContent;
+
+      totalPriceToPay += Number(productPrice);
+
+      if (!productsBought.includes(productName)) {
+         productsBought.push(productName);
+      }
+
+      textAreaElement.textContent += `Added ${productName} for ${productPrice} to the cart.\n`;
+   }
+
+   let checkOutHandler = function (e) {
+      textAreaElement.textContent += `You bought ${productsBought.join(', ')} for ${totalPriceToPay.toFixed(2)}.`
+
+      for (let button of addButtonsElement) {
+         button.disabled = true;
+      }
+      e.target.disabled = true;
+   }
+
+   for (const button of addButtonsElement) {
+      button.addEventListener('click', addProductToCart);
+   }
+
+   checkOutButtonElement.addEventListener('click', checkOutHandler);
 }
