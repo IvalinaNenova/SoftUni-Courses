664d16162edac858bfe59ae7b98c00c8dfbbfbd2
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-23 16:51:47 +0300

    Completed Task 04.Complete dropdown

--- JavaScript Advanced/05.DOM Manipulation and Events/Exercise/04. Fill-Dropdown/dropdown.js
+++ JavaScript Advanced/05.DOM Manipulation and Events/Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,17 @@
 function addItem() {
-    console.log('TODO:...');
+    let newItemTextElement = document.getElementById('newItemText');
+    let newItemValueElement = document.getElementById('newItemValue');
+
+    let text = newItemTextElement.value;
+    let value = newItemValueElement.value;
+
+    let newOptionElement = document.createElement('option');
+    newOptionElement.textContent = text;
+    newOptionElement.value = value;
+
+    let selectElement = document.getElementById('menu');
+    selectElement.appendChild(newOptionElement);
+
+    newItemTextElement.value = '';
+    newItemValueElement.value = '';
 }
