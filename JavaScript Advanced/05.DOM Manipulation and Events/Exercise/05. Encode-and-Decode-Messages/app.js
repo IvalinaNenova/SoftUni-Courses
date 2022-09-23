d6cdc174bdd4cfd987d45571eecbe8e4ab5942d3
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-23 17:39:47 +0300

    Completed Task 05.Encode/Decode Message

--- JavaScript Advanced/05.DOM Manipulation and Events/Exercise/05. Encode-and-Decode-Messages/app.js
+++ JavaScript Advanced/05.DOM Manipulation and Events/Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,35 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let messageElement = document.querySelector('textarea');
+    let encodeButtonElement = document.querySelector('button');
+
+    let receivedMessageElement = document.querySelectorAll('textarea')[1];
+    let decodeButtonElement = document.querySelectorAll('button')[1];
+
+    let onEncode = function () {
+        let givenMessage = messageElement.value;
+        let encodedMessage = '';
+        
+        for (let i = 0; i < givenMessage.length; i++) {
+            let encodedLetter = String.fromCharCode(givenMessage.charCodeAt(i) + 1);
+            encodedMessage += encodedLetter;
+        }
+
+        messageElement.value = '';
+        receivedMessageElement.value = encodedMessage;
+    }
+
+    let onDecode = function () {
+        let receivedMessage = receivedMessageElement.value;
+        let decodedMessage = '';
+
+        for (let i = 0; i < receivedMessage.length; i++) {
+            let decodedLetter = String.fromCharCode(receivedMessage.charCodeAt(i) - 1);
+            decodedMessage += decodedLetter;
+        }
+
+        receivedMessageElement.value = decodedMessage;
+    }
+
+    encodeButtonElement.addEventListener('click', onEncode);
+    decodeButtonElement.addEventListener('click', onDecode);
 }
