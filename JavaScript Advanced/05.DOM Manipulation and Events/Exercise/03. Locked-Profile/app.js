d6140f57a633db81df547dd4390d9094a1e78d6d
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-23 15:43:12 +0300

    Completed Task 03.Locked Profile

--- JavaScript Advanced/05.DOM Manipulation and Events/Exercise/03. Locked-Profile/app.js
+++ JavaScript Advanced/05.DOM Manipulation and Events/Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,22 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttonElements = document.querySelectorAll('div button');
+
+
+    for (let i = 0; i < buttonElements.length; i++) {
+        let button = buttonElements[i];
+        let profileElement = button.parentElement;
+        let lockedElement = profileElement.querySelector('input[value="lock"]');
+        let hiddenInfoElement = document.getElementById(`user${i + 1}HiddenFields`);
+
+        button.addEventListener('click', (e) => {
+
+            if (!lockedElement.checked && button.textContent == 'Show more') {
+                hiddenInfoElement.style.display = 'block';
+                button.textContent = 'Hide it';
+            } else if (!lockedElement.checked && button.textContent == 'Hide it') {
+                hiddenInfoElement.style.display = 'none';
+                button.textContent = 'Show more';
+            }
+        });
+    }
 }
