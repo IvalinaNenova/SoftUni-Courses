c8ab9a4835be5830c2d5d8976bbad3f68bed122c
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-27 18:09:30 +0300

    Completed Task 07.Distance Converter

--- JavaScript Advanced/05.DOM Manipulation and Events/Exercise/07. Distance-Converter/distanceConverter.js
+++ JavaScript Advanced/05.DOM Manipulation and Events/Exercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,31 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    let [inputElement, outputElement] = document.querySelectorAll('input[type="text"]');
+    let buttonElement = document.getElementById('convert');
+
+    let inputUnits = document.getElementById('inputUnits');
+    let outputUnits = document.getElementById('outputUnits');
+
+    let rations = {
+        km: 1000,
+        m: 1,
+        cm: 0.01,
+        mm: 0.001,
+        mi: 1609.34,
+        yrd: 0.9144,
+        ft: 0.3048,
+        in: 0.0254
+    }
+
+    let convert = function (){
+        let valueToConvert = Number(inputElement.value);
+        let unitFrom = inputUnits.value;
+        let unitTo = outputUnits.value;
+
+        let valueToMeters = valueToConvert * rations[unitFrom];
+
+        let result = valueToMeters / rations[unitTo];
+        outputElement.value = result;
+    }
+
+    buttonElement.addEventListener('click', convert);
 }
