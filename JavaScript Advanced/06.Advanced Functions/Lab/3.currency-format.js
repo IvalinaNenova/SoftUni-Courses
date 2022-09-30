33cce212bce9080c27480e4b6ca986d091b4fde9
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-30 14:05:05 +0300

    Completed Task 03.Currency Formatter

---
+++ JavaScript Advanced/06.Advanced Functions/Lab/3.currency-format.js
@@ -0,0 +1,14 @@
+function createFormatter(separator, symbol, symbolFirst, formatter) {
+    return formatter.bind(this, separator, symbol, symbolFirst);
+    //return (value) => formatter(separator, symbol, symbolFirst, value);
+}
+function currencyFormatter(separator, symbol, symbolFirst, value) {
+    let result = Math.trunc(value) + separator;
+    result += value.toFixed(2).substr(-2,2);
+    if (symbolFirst) return symbol + ' ' + result;
+    else return result + ' ' + symbol;
+}
+let dollarFormatter = createFormatter(',', '$', true, currencyFormatter);
+console.log(dollarFormatter(5345));   // $ 5345,00
+console.log(dollarFormatter(3.1429)); // $ 3,14
+console.log(dollarFormatter(2.709));  // $ 2,71
