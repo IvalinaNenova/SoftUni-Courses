97fc77fdf5e3351c68935422a15948b69b883b88
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-30 15:00:23 +0300

    Completed Task 04.Filter Employees

---
+++ JavaScript Advanced/06.Advanced Functions/Lab/4.filter-employees.js
@@ -0,0 +1,55 @@
+function solve(input, chriteria) {
+    let data = JSON.parse(input);
+    let[property, value] = chriteria.split('-');
+    return data.filter(x => x[property] == value)
+    .map((x, i) => `${i}. ${x.first_name} ${x.last_name} - ${x.email}`)
+    .forEach(x => console.log(x));
+}
+solve(`[{
+    "id": "1",
+    "first_name": "Ardine",
+    "last_name": "Bassam",
+    "email": "abassam0@cnn.com",
+    "gender": "Female"
+  }, {
+    "id": "2",
+    "first_name": "Kizzee",
+    "last_name": "Jost",
+    "email": "kjost1@forbes.com",
+    "gender": "Female"
+  },  
+{
+    "id": "3",
+    "first_name": "Evanne",
+    "last_name": "Maldin",
+    "email": "emaldin2@hostgator.com",
+    "gender": "Male"
+  }]`, 
+'gender-Female');
+
+solve(`[{
+    "id": "1",
+    "first_name": "Kaylee",
+    "last_name": "Johnson",
+    "email": "k0@cnn.com",
+    "gender": "Female"
+  }, {
+    "id": "2",
+    "first_name": "Kizzee",
+    "last_name": "Johnson",
+    "email": "kjost1@forbes.com",
+    "gender": "Female"
+  }, {
+    "id": "3",
+    "first_name": "Evanne",
+    "last_name": "Maldin",
+    "email": "emaldin2@hostgator.com",
+    "gender": "Male"
+  }, {
+    "id": "4",
+    "first_name": "Evanne",
+    "last_name": "Johnson",
+    "email": "ev2@hostgator.com",
+    "gender": "Male"
+  }]`,
+ 'last_name-Johnson');
