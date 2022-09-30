c63f8d703f28dac6255bb7434be79a60dd6aad11
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-30 11:55:46 +0300

    Completed Task 01. Area and volume calculator

---
+++ JavaScript Advanced/06.Advanced Functions/Lab/1.area-and-volume-calculator.js
@@ -0,0 +1,36 @@
+function area() {
+    return Math.abs(this.x * this.y);
+};
+
+function vol() {
+    return Math.abs(this.x * this.y * this.z);
+};
+
+// function solve(area, vol, input) {
+//     let points = JSON.parse(input);
+//     let result = [];
+
+//     for (const coordinates of points) {
+//         result.push({
+//             area: area.call(coordinates),
+//             volume: vol.call(coordinates)
+//         })
+//     }
+//     return result;
+// }
+    function fancySolve(area, vol, input) {
+        return JSON.parse(input)
+        .map(x=> ({
+            area: area.call(x),
+            volume: vol.call(x)
+        }));
+    }
+
+console.log(fancySolve(area, vol, `[
+    {"x":"10","y":"-22","z":"10"},
+    {"x":"47","y":"7","z":"-5"},
+    {"x":"55","y":"8","z":"0"},
+    {"x":"100","y":"100","z":"100"},
+    {"x":"55","y":"80","z":"250"}
+    ]`
+    ));
