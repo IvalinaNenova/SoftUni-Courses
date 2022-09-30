825cf103b89526f30582257bb1b4cf78ea05c19d
Ivalina Nenova <nenovaivalina@gmail.com>
2022-09-30 11:56:03 +0300

    Completed Task 02. Add

---
+++ JavaScript Advanced/06.Advanced Functions/Lab/2.add.js
@@ -0,0 +1,11 @@
+function solution(number) {
+    return (a) => a + number;
+}
+
+let add5 = solution(5);
+console.log(add5(2));
+console.log(add5(3));
+
+let add7 = solution(7);
+console.log(add7(2));
+console.log(add7(3));
