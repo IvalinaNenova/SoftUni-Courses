6ae9149ae17a5c05d96b46d4c0780f54ecb5dbc7
Ivalina Nenova <nenovaivalina@gmail.com>
2022-10-01 17:14:46 +0300

    Completed Task 07.Simple Calculator

--- JavaScript Advanced/06.Advanced Functions/Exercise/07. Simple Calculator/app.js
+++ JavaScript Advanced/06.Advanced Functions/Exercise/07. Simple Calculator/app.js
@@ -1,7 +1,30 @@
 function calculator() {
-    // TODO:
+    let input1;
+    let input2;
+    let result;
+
+    function init(selector1, selector2, selector3) {
+        input1 = document.querySelector(selector1);
+        input2 = document.querySelector(selector2);
+        result = document.querySelector(selector3);
+    };
+
+    function add() {
+        result.value = Number(input1.value) + Number(input2.value);
+    };
+
+    function subtract() {
+        result.value = Number(input1.value) - Number(input2.value);
+    }
+
+
+    let obj = {
+        init,
+        add,
+        subtract
+    };
+    return obj;
 }
 
-
-
-
+const calculate = calculator();
+calculate.init('#num1', '#num2', '#result');
