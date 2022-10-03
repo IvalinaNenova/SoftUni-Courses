39d859eaa919bcae83290827c8caca359dcf35df
Ivalina Nenova <nenovaivalina@gmail.com>
2022-10-03 22:12:29 +0300

    Completed Task 09.Task Manager 100/100

--- JavaScript Advanced/06.Advanced Functions/Exercise/09. Task Manager/app.js
+++ JavaScript Advanced/06.Advanced Functions/Exercise/09. Task Manager/app.js
@@ -1,55 +1,75 @@
 function solve(e) {
     document.querySelector('#add').addEventListener('click', createTask);
     let sections = document.getElementsByTagName('section');
-    let addSection = sections[0];
     let openSection = sections[1];
     let inProgress = sections[2];
     let complete = sections[3];
+    let buttons = {
+        open: { green: 'Start', red: 'Delete' },
+        inProgress: { red: 'Delete', orange: 'Finish' },
+    }
 
     function createTask(e) {
         e.preventDefault();
 
-        let form = e.target.parentNode;
-        let data  = Array.from(form.elements).map(x => x.value).slice(0, 3);
         let elements = ['h3', 'p', 'p'];
-        let buttons = {
-            open: {green: 'Start', red: 'Delete'},
-            inProgress: {red: 'Delete', orange: 'Finish'},
-        }
-        let article = generateArticle(elements, data, buttons);
-        console.log(article);
-        openSection.children[1].appendChild(article);
-        console.log(article.parentNode.parentNode);
+        let form = e.target.parentNode;
+        let title = form.elements['task'].value;
+        let description = 'Description: ' + form.elements['description'].value
+        let date = 'Due Date: ' + form.elements['date'].value;
 
-        let actions = eventDelegator();
-        article.addEventListener('click', eventDelegator);
-       
-    }
-
-    function eventDelegator(e){
-        if (e.target.tagName != 'BUTTON') {
+        let data = [title, description, date];
+        if (data.includes('')) {
             return;
         }
+
+        let article = generateArticle(elements, data, buttons);
+        openSection.children[1].appendChild(article);
+
+        article.addEventListener('click', (ev) => {
+            let actions = eventDelegator(buttons);
+            if (ev.target.tagName != 'BUTTON') {
+                return;
+            }
+            if (ev.target.textContent == 'Start') {
+                console.log(ev.target.parentNode);
+                ev.target.parentNode.remove();
+                actions['start'](ev);
+            } else if (ev.target.textContent == 'Finish') {
+                ev.target.parentNode.remove();
+                actions['finish'](ev);
+            } else if (ev.target.textContent == 'Delete') {
+                actions['delete'](ev)
+            }
+        });
+    }
+
+    function eventDelegator(buttons) {
         let functions = {
             start: (e) => {
-                
-                console.log(e.currentTarget)
-                e.currentTarget.removeElement('div');
                 e.currentTarget.appendChild(generateButtons(buttons['inProgress']))
+                inProgress.children[1].appendChild(e.currentTarget);
+            },
+            finish: (e) => {
+                complete.children[1].appendChild(e.currentTarget);
+            },
+            delete: (e) => {
+                e.currentTarget.remove();
             }
         }
 
         return functions;
     }
+
     function generateArticle(elements, data, buttons) {
         let articleElement = document.createElement('article');
 
-        for(let i = 0; i < elements.length; i++) {
+        for (let i = 0; i < elements.length; i++) {
             let cell = document.createElement(elements[i]);
             cell.textContent = data[i];
             articleElement.appendChild(cell);
         }
-            articleElement.appendChild(generateButtons(buttons['open']));
+        articleElement.appendChild(generateButtons(buttons['open']));
         return articleElement;
     }
 
@@ -65,82 +85,4 @@ function solve(e) {
         }
         return buttonDiv;
     }
-
-    // let divsElement = document.querySelectorAll('section div:nth-of-type(2)');
-    // let formElement = document.querySelector('form');
-    // let task = formElement.querySelector('#task');
-    // let description = formElement.querySelector('#description');
-    // let date = formElement.querySelector('#date');
-
-    // let addButton = document.querySelector('#add');
-    // addButton.addEventListener('click', onClick);
-
-    // function onClick(e) {
-    //     e.preventDefault();
-    //     let open = {
-    //         title: { 'h3': task.value },
-    //         paragraph1: { 'p': `Description: ${description.value}` },
-    //         paragraph2: { 'p': `Due Date: ${date.value}` },
-    //         buttonsDiv: { 'div': { 'green': 'Start', 'red': 'Delete' } },
-    //         display: 1
-    //     }
-    //     let inProgress = {
-    //         title: { 'h3': task.value },
-    //         paragraph1: { 'p': `Description: ${description.value}` },
-    //         paragraph2: { 'p': `Due Date: ${date.value}` },
-    //         buttonsDiv: { 'div': { 'red': 'Delete', 'orange': 'Finish' } },
-    //         display: 2
-    //     }
-    //     let complete = {
-    //         title: { 'h3': task.value },
-    //         paragraph1: { 'p': `Description: ${description.value}` },
-    //         paragraph2: { 'p': `Due Date: ${date.value}` },
-    //         display: 3
-    //     }
-
-    //     if (e.target.id === 'add') {
-    //         if (!task.value || !description.value || !date.value) {
-    //             return;
-    //         }
-    //         createArticle(open);
-    //     }
-    //     else if (e.target.textContent === 'Start') {
-    //         e.target.parentNode.parentNode.remove('article');
-    //         createArticle(inProgress)
-    //     } else if (e.target.textContent === 'Finish') {
-    //         e.target.parentNode.parentNode.remove('article');
-    //         createArticle(complete);
-    //     } else if (e.target.textContent === 'Delete') {
-    //         e.target.parentNode.parentNode.remove('article');
-    //     }
-    // }
-    // function createArticle(obj) {
-    //     let articleElement = document.createElement('article');
-    //     let info = Object.values(obj);
-    //     for (const data of info) {
-    //         for (const el in data) {
-    //             articleElement.appendChild(createCell(el, data[el]));
-    //         }
-    //     }
-    //     divsElement[Number(obj.display)].appendChild(articleElement);
-    // }
-
-    // let createCell = function createChildrean(element, data) {
-    //     let cell;
-    //     if (element != 'div') {
-    //         cell = document.createElement(element);
-    //         cell.textContent = data;
-    //     } else {
-    //         cell = document.createElement(element);
-    //         cell.classList.add('flex');
-    //         for (const el in data) {
-    //             let buttonElement = document.createElement('button');
-    //             buttonElement.addEventListener('click', onClick);
-    //             buttonElement.classList.add(el)
-    //             buttonElement.textContent = data[el];
-    //             cell.appendChild(buttonElement)
-    //         }
-    //     }
-    //     return cell;
-   // }
 }
