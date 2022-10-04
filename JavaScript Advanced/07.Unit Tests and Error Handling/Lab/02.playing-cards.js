12f4ed23c75c9193ffbbdecafda52a5d18b49386
Ivalina Nenova <nenovaivalina@gmail.com>
2022-10-04 12:43:40 +0300

    Completed Task 02.Playing Cards 75/100

---
+++ JavaScript Advanced/07.Unit Tests and Error Handling/Lab/02.playing-cards.js
@@ -0,0 +1,23 @@
+function solve(face, suit) {
+    let validFaces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    let suits = {
+        S: '\u2660',
+        H: '\u2665',
+        D: '\u2666',
+        C: '\u2663',
+    }
+    if (!validFaces.includes(face) || suits[suit] == undefined) {
+        throw new Error('Error');
+    }
+
+    let card = {
+        face,
+        suit: suits[suit],
+        toString() { console.log(this.face + this.suit) }
+    }
+    return card;
+}
+
+solve('2', 'H').toString()
+solve('K', 'C').toString()
+solve('D', '10').toString()
