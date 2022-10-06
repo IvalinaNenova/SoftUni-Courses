1457bf97601b047f763af24428e410e97ad0dc5f
Ivalina Nenova <nenovaivalina@gmail.com>
2022-10-06 17:51:00 +0300

    Completed Task 02.Get Persons

---
+++ JavaScript Advanced/08.Classes and Attributes/Lab/02.get-persons.js
@@ -0,0 +1,23 @@
+function solve() {
+    class Person {
+        constructor(firstName, lastName, age, email) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+        }
+
+        toString() {
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+        }
+    }
+
+    let person1 = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
+    let person2 = new Person('SoftUni');
+    let person3 = new Person('Stephan', 'Johnson', 25)
+    let person4 = new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com');
+
+    let result = [];
+    result.push(person1, person2, person3, person4);
+    return result;
+}
