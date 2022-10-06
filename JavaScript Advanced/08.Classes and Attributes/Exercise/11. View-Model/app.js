740e50d7c9f979fc5b1bb0258c27595abd406078
Ivalina Nenova <nenovaivalina@gmail.com>
2022-10-06 17:24:21 +0300

    Created folder for Classes and Attributes Homework

---
+++ JavaScript Advanced/08.Classes and Attributes/Exercise/11. View-Model/app.js
@@ -0,0 +1,8 @@
+class Textbox {
+    //TODO
+}
+
+let textbox = new Textbox(".textbox",/[^a-zA-Z0-9]/);
+let inputs = document.getElementsByClassName('.textbox');
+
+inputs.addEventListener('click',function(){console.log(textbox.value);});
