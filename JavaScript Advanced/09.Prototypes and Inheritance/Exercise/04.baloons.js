34d2d5888542174e3d58e215bc3ef58c24198675
Ivalina Nenova <nenovaivalina@gmail.com>
2022-10-11 14:56:53 +0300

    Completed Task 04.Baloons

---
+++ JavaScript Advanced/09.Prototypes and Inheritance/Exercise/04.baloons.js
@@ -0,0 +1,42 @@
+function solution() {
+    class Balloon {
+        constructor(color, hasWeight) {
+            this.color = color;
+            this.hasWeight = hasWeight;
+        }
+    }
+
+    class PartyBalloon extends Balloon {
+        constructor(color, hasWeight, ribbonColor, ribbonLength) {
+            super(color, hasWeight);
+            this._ribbon = {
+                color: ribbonColor,
+                length: ribbonLength
+            }
+        }
+        get ribbon() {
+            return this._ribbon;
+        }
+    }
+
+    class BirthdayBalloon extends PartyBalloon {
+        constructor(color, hasWeight, ribbonColor, ribbonLength, text) {
+            super(color, hasWeight, ribbonColor, ribbonLength)
+            this.text = text;
+        }
+    };
+
+    return {
+        Balloon,
+        PartyBalloon,
+        BirthdayBalloon
+    }
+}
+let classes = solution();
+let testBalloon = new classes.Balloon("yellow", 20.5);
+let testPartyBalloon = new classes.PartyBalloon("yellow", 20.5, "red", 10.25);
+let ribbon = testPartyBalloon.ribbon;
+console.log(testBalloon);
+console.log(testPartyBalloon);
+console.log(ribbon);
+
