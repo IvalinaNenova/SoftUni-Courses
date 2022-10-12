c0390df34f8d0934b9a3a0cae021ceedb4107727
Ivalina Nenova <nenovaivalina@gmail.com>
2022-10-12 21:06:17 +0300

    Refactored Code

--- JavaScript Advanced/Exam Practice/Regular Exam - 25 Jun 2022/01.Car Dealers/app.js
+++ JavaScript Advanced/Exam Practice/Regular Exam - 25 Jun 2022/01.Car Dealers/app.js
@@ -1,75 +1,67 @@
-window.addEventListener("load", solve);
+window.addEventListener("load", solve)
 
 function solve() {
-  let form = document.querySelector("form");
-  let tableBodyElement = document.querySelector('#table-body');
-  let ulElement = document.querySelector('#cars-list');
-  let profitElement = document.querySelector('#profit');
-  let profitValue = 0;
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    let inputElements = Array.from(form.elements).slice(1, -1);
-    let inputValues = Array.from(form.elements).slice(1, -1).map(input => input.value);
+    let form = document.querySelector("form");
+    let tableBodyElement = document.querySelector('#table-body');
+    let ulElement = document.querySelector('#cars-list');
+    let profitElement = document.querySelector('#profit');
+    let profitValue = 0;
 
-    if (inputValues.includes('') || inputValues[4] > inputValues[5]) {
-      return;
-    }
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        let inputElements = Array.from(form.elements).slice(1, -1);
+        let inputValues = inputElements.map(input => input.value);
 
-    let tableRow = document.createElement('tr');
-    tableRow.classList.add('row');
-    for (const value of inputValues) {
-      let dataCell = document.createElement('td');
-      dataCell.textContent = value;
-      tableRow.appendChild(dataCell);
-    }
-    let editButton = document.createElement('button');
-    editButton.setAttribute('class', 'action-btn edit');
-    editButton.textContent = 'Edit';
+        let tableRow = createTag('tr', null, 'row');
+        let editButton = createTag('button', 'Edit', 'action-btn edit');
+        let sellButton = createTag('button', 'Sell', 'action-btn sell');
 
-    editButton.addEventListener('click', (e) => {
-      for (let i = 0; i < inputValues.length; i++) {
-        inputElements[i].value = inputValues[i];
-      }
-      tableRow.remove();
+        if (inputValues.includes('') || inputValues[4] > inputValues[5]) {
+            return;
+        }
+
+        for (const value of inputValues) {
+            let dataCell = createTag('td', value);
+            tableRow.appendChild(dataCell);
+        }
+
+        editButton.addEventListener('click', (e) => {
+            for (let i = 0; i < inputValues.length; i++) {
+                inputElements[i].value = inputValues[i];
+            }
+            tableRow.remove();
+        });
+
+        sellButton.addEventListener('click', (e) => {
+            let [make, model, year, fuel, originalCost, sellingPrice] = inputValues;
+
+            let liElement = createTag('li', null, 'each-list');
+            liElement.appendChild(createTag('span', make + ' ' + model));
+            liElement.appendChild(createTag('span', year))
+            liElement.appendChild(createTag('span', sellingPrice - originalCost))
+
+            ulElement.appendChild(liElement);
+            profitValue += sellingPrice - originalCost;
+            profitElement.textContent = profitValue.toFixed(2);
+
+            tableRow.remove();
+        });
+
+        let buttonsCell = document.createElement('td');
+        buttonsCell.appendChild(editButton);
+        buttonsCell.appendChild(sellButton);
+        tableRow.appendChild(buttonsCell);
+        tableBodyElement.appendChild(tableRow);
+
+        inputElements.forEach(el => el.value = '');
+
+        function createTag(tag, text = null, className = null, id = null, type = null) {
+            let el = document.createElement(tag);
+            if (text) { el.textContent = text; }
+            if (type) { el.type = type; }
+            if (id) { el.id = id; }
+            if (className) { el.className = className; }
+            return el;
+        }
     });
-
-    let sellButton = document.createElement('button');
-    sellButton.setAttribute('class', 'action-btn sell')
-    sellButton.textContent = 'Sell';
-
-    sellButton.addEventListener('click', (e) => {
-      let [make, model, year, fuel, originalCost, sellingPrice] = inputValues;
-      let liElement = document.createElement('li');
-      liElement.setAttribute('class', 'each-list');
-      let span1 = document.createElement('span');
-      span1.textContent = `${make} ${model}`;
-      let span2 = document.createElement('span');
-      span2.textContent = year;
-      let span3 = document.createElement('span');
-      span3.textContent = sellingPrice - originalCost;
-
-      liElement.appendChild(span1)
-      liElement.appendChild(span2);
-      liElement.appendChild(span3);
-
-      ulElement.appendChild(liElement);
-      profitValue += Number(sellingPrice) - Number(originalCost);
-      profitElement.textContent = profitValue.toFixed(2);
-      tableRow.remove();
-
-    });
-
-
-    let buttonsCell = document.createElement('td');
-    buttonsCell.appendChild(editButton);
-    buttonsCell.appendChild(sellButton);
-    tableRow.appendChild(buttonsCell);
-    tableBodyElement.appendChild(tableRow);
-
-    for (const el of inputElements) {
-      el.value = '';
-    }
-
-    console.log(inputValues);
-  });
 }
