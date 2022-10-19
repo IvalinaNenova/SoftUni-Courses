ff3aa65aef02c119323cd79642c318ccae419373
Ivalina Nenova <nenovaivalina@gmail.com>
2022-10-19 22:18:35 +0300

    Task 01.Christmas gifts - unfinished

--- JavaScript Advanced/Exam Practice/Exam Prep - 15 Feb 2021/01.Christmas Gifts Delivery/app.js
+++ JavaScript Advanced/Exam Practice/Exam Prep - 15 Feb 2021/01.Christmas Gifts Delivery/app.js
@@ -1,3 +1,31 @@
 function solution() {
-    //TO DO
+    let [listSection, sentSection, discardSection] = Array.from(document.querySelectorAll('section ul'));
+    console.log(listSection, sentSection, discardSection);
+    let inputElement = document.querySelector('input');
+    let addButton = document.querySelector('button');
+    let listOfGifts = []
+    addButton.addEventListener('click', (e)=>{
+        let giftName = inputElement.value;
+        let listItem = createTag('li', giftName, 'gift');
+        let sendButton = createTag('button', 'Send', null, 'sendButton');
+        let discardButton = createTag('button', 'Discard', null, 'discardButton');
+        listItem.appendChild(sendButton);
+        listItem.appendChild(discardButton);
+        listSection.appendChild(listItem);
+         
+        
+
+
+
+        inputElement.value = '';
+    });
+
+    function createTag(tag, text = null, className = null, id = null, type = null) { 
+        let el = document.createElement(tag); 
+        if (text) { el.textContent = text; } 
+        if (type) { el.type = type; } 
+        if (id) { el.id = id; } 
+        if (className) { el.className = className; } 
+        return el; 
+    } 
 }
