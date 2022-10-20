b574855c1151f0c8bd43ac9bbc7fa1611cd009c6
Ivalina Nenova <nenovaivalina@gmail.com>
2022-10-20 17:40:47 +0300

    Completed Task 01.Christmas Gifts List

--- JavaScript Advanced/Exam Practice/Exam Prep - 15 Feb 2021/01.Christmas Gifts Delivery/app.js
+++ JavaScript Advanced/Exam Practice/Exam Prep - 15 Feb 2021/01.Christmas Gifts Delivery/app.js
@@ -1,31 +1,49 @@
 function solution() {
     let [listSection, sentSection, discardSection] = Array.from(document.querySelectorAll('section ul'));
-    console.log(listSection, sentSection, discardSection);
     let inputElement = document.querySelector('input');
     let addButton = document.querySelector('button');
-    let listOfGifts = []
-    addButton.addEventListener('click', (e)=>{
+    let allGifts = [];
+
+    addButton.addEventListener('click', () => {
+
         let giftName = inputElement.value;
+
         let listItem = createTag('li', giftName, 'gift');
+
         let sendButton = createTag('button', 'Send', null, 'sendButton');
+        sendButton.addEventListener('click', onSend);
+
         let discardButton = createTag('button', 'Discard', null, 'discardButton');
+        discardButton.addEventListener('click', onDiscard);
+
         listItem.appendChild(sendButton);
         listItem.appendChild(discardButton);
-        listSection.appendChild(listItem);
-         
-        
 
-
+        allGifts.push(listItem);
+        allGifts.sort((a, b) => a.textContent.localeCompare(b.textContent))
+            .forEach(li => listSection.appendChild(li));
 
         inputElement.value = '';
+
+        function onSend() {
+            listItem.removeChild(sendButton);
+            listItem.removeChild(discardButton);
+            sentSection.appendChild(listItem);
+            allGifts = allGifts.filter(li => li != listItem);
+        };
+        function onDiscard() {
+            listItem.removeChild(sendButton);
+            listItem.removeChild(discardButton);
+            discardSection.appendChild(listItem);
+            allGifts = allGifts.filter(li => li != listItem);
+        };
     });
 
-    function createTag(tag, text = null, className = null, id = null, type = null) { 
-        let el = document.createElement(tag); 
-        if (text) { el.textContent = text; } 
-        if (type) { el.type = type; } 
-        if (id) { el.id = id; } 
-        if (className) { el.className = className; } 
-        return el; 
-    } 
+    function createTag(tag, text = null, className = null, id = null) {
+        let el = document.createElement(tag);
+        if (text) { el.textContent = text; }
+        if (id) { el.id = id; }
+        if (className) { el.className = className; }
+        return el;
+    }
 }
