f4cc7e07b6cc9c7bb9368d8149ad4b7f315c86de
Ivalina Nenova <nenovaivalina@gmail.com>
2022-10-30 12:22:26 +0200

    Completed Task 01.Bus stop - 75/100

--- JavaScript Applications/01.Asynchronous Programming/Exercise/01.Bus-Stop/app.js
+++ JavaScript Applications/01.Asynchronous Programming/Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,26 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    let stopId = document.getElementById('stopId').value;
+    const stopName = document.getElementById('stopName');
+    const listOfStops = document.getElementById('buses');
+
+    try {
+        let result = await fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`);
+
+        if (result.ok == false || result.status != 200) {
+            throw new Error('Error')
+        }
+
+        listOfStops.innerHTML = '';
+        let data = await result.json();
+
+        stopName.textContent = data.name;
+        for (const bus in data.buses) {
+            let row = document.createElement('li');
+            row.textContent = `Bus ${bus} arrives in ${data.buses[bus]} minutes`;
+            listOfStops.appendChild(row);
+        }
+    } catch (error) {
+        stopName.textContent = error.message;
+        listOfStops.innerHTML = '';
+    }
 }
