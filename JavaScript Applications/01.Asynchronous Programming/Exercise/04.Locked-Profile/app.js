1c975b173e43d3f8014057ac5a3e30d797c0b57c
Ivalina Nenova <nenovaivalina@gmail.com>
2022-11-01 13:36:48 +0200

    Completed Task 04.Locked Profile

--- JavaScript Applications/01.Asynchronous Programming/Exercise/04.Locked-Profile/app.js
+++ JavaScript Applications/01.Asynchronous Programming/Exercise/04.Locked-Profile/app.js
@@ -1,3 +1,49 @@
-function lockedProfile() {
-    console.log('TODO...')
+async function lockedProfile() {
+    let mainDiv = document.querySelector('#main');
+    mainDiv.innerHTML = '';
+
+    let result = await fetch('http://localhost:3030/jsonstore/advanced/profiles');
+    let profileData = Object.values(await result.json());
+
+    profileData.forEach(user => {
+        let userCard = document.createElement('div');
+        userCard.className = 'profile';
+        userCard.innerHTML = `<img src="./iconProfile2.png" class="userIcon" />
+                              <label>Lock</label>
+                              <input type="radio" name="user${user._id}Locked" value="lock" checked>
+                              <label>Unlock</label>
+                              <input type="radio" name="user${user._id}Locked" value="unlock"><br>
+                              <hr>
+                              <label>Username</label>
+                              <input type="text" name="user${user._id}Username" value="${user.username}" disabled readonly />
+                              <div id="user1HiddenFields" style="display:none">
+                                  <hr>
+                                  <label>Email:</label>
+                                  <input type="email" name="user${user._id}Email" value="${user.email}" disabled readonly />
+                                  <label>Age:</label>
+                                  <input type="email" name="user${user._id}Age" value="${user.age}" disabled readonly />
+                              </div>
+                              <button>Show more</button>`;
+        mainDiv.appendChild(userCard);
+     });
+
+    let buttonElements = document.querySelectorAll('div button');
+    for (let i = 0; i < buttonElements.length; i++) {
+        let button = buttonElements[i];
+        let profileElement = button.parentElement;
+        console.log(button.parentElement);
+        let lockedElement = profileElement.querySelector('input[value="lock"]');
+        let hiddenInfoElement = profileElement.querySelector(`#user1HiddenFields`);
+
+        button.addEventListener('click', (e) => {
+
+            if (!lockedElement.checked && button.textContent == 'Show more') {
+                hiddenInfoElement.style.display = 'block';
+                button.textContent = 'Hide it';
+            } else if (!lockedElement.checked && button.textContent == 'Hide it') {
+                hiddenInfoElement.style.display = 'none';
+                button.textContent = 'Show more';
+            }
+        });
+    }
 }
