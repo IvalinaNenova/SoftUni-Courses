c8f47889c6fb9edbce69b10cdf52599550bc9d89
Ivalina Nenova <nenovaivalina@gmail.com>
2022-11-01 10:22:48 +0200

    Completed Task 03.Forecaster

--- JavaScript Applications/01.Asynchronous Programming/Exercise/03.Forecaster/app.js
+++ JavaScript Applications/01.Asynchronous Programming/Exercise/03.Forecaster/app.js
@@ -1,5 +1,22 @@
 function attachEvents() {
-    console.log("TODO...");
+    const baseUrl = 'http://localhost:3030/jsonstore/forecaster';
+    const forecastDiv = document.querySelector('#forecast');
+    let submitButton = document.querySelector('#submit');
+    submitButton.addEventListener('click', onClick);
+    async function onClick() {
+        let location = document.querySelector('#location').value;
+
+        let result = await fetch(`${baseUrl}/locations`);
+        let data = await result.json();
+        console.log(data);
+        let searched = data.find(l => l.name == location);
+        console.log(searched);
+        console.log(searched.code);
+
+        let todayForecast = await fetch(`${baseUrl}/today/${searched.code}`);
+        let todaysData = await todayForecast.json();
+        console.log(todaysData);
+    }
 }
 
 attachEvents();
