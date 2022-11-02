d3656ca223223fb33d3427d25e905e41334c6ad3
Ivalina Nenova <nenovaivalina@gmail.com>
2022-11-02 14:08:23 +0200

    Completed Task 01.Messenger

--- JavaScript Applications/02.Data and Authentication/01.Messenger/app.js
+++ JavaScript Applications/02.Data and Authentication/01.Messenger/app.js
@@ -1,5 +1,36 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('submit')
+    .addEventListener('click', onSubmit)
+
+    document.getElementById('refresh')
+    .addEventListener('click', onRefresh);
+
+    const displayArea = document.getElementById('messages');
+
+    async function onSubmit(){
+        let author = document.querySelector('[name="author"]').value;
+        let content = document.querySelector('[name="content"]').value;
+
+        await fetch(`http://localhost:3030/jsonstore/messenger`, {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({author, content})
+        })
+        console.log(author, content);
+    }
+
+    async function onRefresh() {
+        let response = await fetch(`http://localhost:3030/jsonstore/messenger`);
+        let data = await response.json();
+        let output = [];
+        
+        for (const message of Object.values(data)) {
+            output.push(`${message.author}: ${message.content}`);
+        }
+        displayArea.value = output.join('\n');
+
+        console.log(data);
+    }
 }
 
 attachEvents();
