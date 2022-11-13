8c0530a07ef99ae9a233436d7fed3a2810867ff9
Ivalina Nenova <nenovaivalina@gmail.com>
2022-11-13 13:55:34 +0200

    Completed Task 03.Search in List

--- JavaScript Applications/05.Client Side Rendering/03.Search-in-List/search.js
+++ JavaScript Applications/05.Client Side Rendering/03.Search-in-List/search.js
@@ -1,3 +1,28 @@
+import {html, render} from './node_modules/lit-html/lit-html.js';
+import {towns} from './towns.js';
+const townsDiv = document.querySelector('#towns');
+
+
+let townsTemplate = html`
+<ul>
+   ${towns.map(town => html`<li>${town}</li>`)}
+</ul>`
+
+render(townsTemplate, townsDiv);
+
+document.querySelector('button').addEventListener('click', search);
 function search() {
-   // TODO
+   let input = document.getElementById('searchText').value;
+   let listItems = Array.from(townsDiv.children[0].children);
+
+   let count = 0
+   listItems.forEach(li => {
+      if (li.textContent.includes(input)) {
+         li.setAttribute('class', 'active');
+         count++;
+      }else{
+         li.removeAttribute('class', 'active');
+      }
+   })
+   document.querySelector('#result').textContent = `${count} matches found`;
 }
