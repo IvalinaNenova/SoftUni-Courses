423cad11f85b5437bedd2fdf5bfd753ca979d9e0
Ivalina Nenova <nenovaivalina@gmail.com>
2023-01-31 18:57:28 +0200

    Completed Task 8

--- MS SQL/07.Functions and Stored Procedures/Tasks 1-7.sql
+++ MS SQL/07.Functions and Stored Procedures/Tasks 1-7.sql
@@ -146,8 +146,42 @@ Hint:
 You may set ManagerID column in Departments table to nullable (using query "ALTER TABLE …").
 */
 
-ALTER TABLE Departments
-ALTER COLUMN ManagerID INT NULL
+GO
 
-SELECT * FROM Departments
+CREATE OR ALTER PROCEDURE [usp_DeleteEmployeesFromDepartment](@departmentId INT)
+AS
+BEGIN
+	 DELETE FROM [EmployeesProjects]
+		   WHERE [EmployeeID] IN (
+								  SELECT [EmployeeID]
+								    FROM [Employees]
+							       WHERE [DepartmentID] = @departmentId
+								 )
+	 UPDATE [Employees]
+	    SET [ManagerID] = NULL
+	  WHERE [ManagerID] IN (
+							 SELECT [EmployeeID]
+							   FROM [Employees]
+							  WHERE [DepartmentID] = @departmentId
+						   )
+	  ALTER TABLE [Departments]
+	 ALTER COLUMN [ManagerID] INT
+
+	 UPDATE [Departments]
+		SET [ManagerID] = NULL
+	  WHERE [ManagerID] IN (
+							 SELECT [EmployeeID]
+							   FROM [Employees]
+							  WHERE [DepartmentID] = @departmentId
+						   )
+	 DELETE FROM [Employees]
+		   WHERE [DepartmentID] = @departmentId
+
+	 DELETE FROM [Departments]
+	       WHERE [DepartmentID] = @departmentId
+
+	 SELECT COUNT([EmployeeId])
+	   FROM [Employees]
+	  WHERE [DepartmentID] = @departmentId
+END
 
