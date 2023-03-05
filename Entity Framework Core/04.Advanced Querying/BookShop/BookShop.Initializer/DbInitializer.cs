71d5daf5e9c1225a585f1d5fe1d73757a8ea1ab5
Ivalina Nenova <nenovaivalina@gmail.com>
2023-03-05 14:13:45 +0200

    Completed problem 02.BooksByAgerestriction

---
+++ Entity Framework Core/04.Advanced Querying/BookShop/BookShop.Initializer/DbInitializer.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+namespace BookShop.Initializer
+{
+    using System;
+
+    using Data;
+    using Models;
+    using Generators;
+
+    public class DbInitializer
+    {
+        public static void ResetDatabase(BookShopContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            Console.WriteLine("BookShop database created successfully.");
+
+            Seed(context);
+        }
+
+        public static void Seed(BookShopContext context)
+        {
+            Book[] books = BookGenerator.CreateBooks();
+
+            context.Books.AddRange(books);
+
+            context.SaveChanges();
+
+            Console.WriteLine("Sample data inserted successfully.");
+        }
+    }
+}
---
+++ Entity Framework Core/04.Advanced Querying/BookShop/BookShop/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using BookShop.Models.Enums;
+
+namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            //string command = Console.ReadLine();
+
+            //string result = GetBooksByAgeRestriction(db, command);
+            //Console.WriteLine(result);
+        }
+
+        //Problem 02
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            AgeRestriction ageRestriction;
+
+            var isParseSuccess = Enum.TryParse<AgeRestriction>(command, true, out ageRestriction);
+
+            if (!isParseSuccess)
+            {
+                return string.Empty;
+            }
+
+            string[] bookTitles = context
+                .Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .Select(b => b.Title)
+                .OrderBy(t => t)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, bookTitles);
+        }
+
+        //Problem 03
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+
+        }
+    }
+}
+
+
