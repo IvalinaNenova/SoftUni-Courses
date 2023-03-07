e9f451a7988a83f423ce2d71995a89d42274ded5
Ivalina Nenova <nenovaivalina@gmail.com>
2023-03-07 11:46:22 +0200

    Completed Task 15. Increase Prices

--- Entity Framework Core/04.Advanced Querying/BookShop/BookShop/StartUp.cs
+++ Entity Framework Core/04.Advanced Querying/BookShop/BookShop/StartUp.cs
@@ -292,6 +292,18 @@ namespace BookShop
         }
 
         //problem 15
+        public static void IncreasePrices(BookShopContext context)
+        {
+            var booksBefore2010 = context
+                .Books
+                .Where(b => b.ReleaseDate.Value.Year < 2010);
 
+            foreach (var book in booksBefore2010)
+            {
+                book.Price += 5;
+            }
+
+            context.SaveChanges();
+        }
     }
 }
